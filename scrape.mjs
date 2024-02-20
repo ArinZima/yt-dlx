479c37af5f24b95e359ebe9f96cb2158842b673e
Shovit Dutta <shovitdutta@proton.me>
2024-02-20 22:24:29 +0530

    yt-core + mixly

--- scrape.mjs
+++ scrape.mjs
@@ -1,4 +1,4 @@
-import { chromium, firefox, webkit } from "playwright";
+import { chromium } from "playwright";
 
 async function Chromium() {
   const browser = await chromium.launch();
@@ -15,44 +15,6 @@ async function Chromium() {
   await browser.close();
 }
 
-async function Firefox() {
-  const browser = await firefox.launch();
-  const page = await browser.newPage();
-  await page.goto("https://openai.com/blog/");
-  await page.waitForLoadState("domcontentloaded");
-  const articleTitles = await page.$$eval(".article-card h2", (elements) =>
-    elements.map((element) => element.textContent.trim())
-  );
-  console.log("Article Titles:");
-  articleTitles.forEach((title, index) => {
-    console.log(`${index + 1}. ${title}`);
-  });
-  await browser.close();
-}
-
-async function Webkit() {
-  const browser = await webkit.launch();
-  const page = await browser.newPage();
-  await page.goto("https://openai.com/blog/");
-  await page.waitForLoadState("domcontentloaded");
-  const articleTitles = await page.$$eval(".article-card h2", (elements) =>
-    elements.map((element) => element.textContent.trim())
-  );
-  console.log("Article Titles:");
-  articleTitles.forEach((title, index) => {
-    console.log(`${index + 1}. ${title}`);
-  });
-  await browser.close();
-}
-
-(async () => {
-  await Chromium().catch((error) => {
-    console.error("Error during scraping:", error);
-  });
-  await Firefox().catch((error) => {
-    console.error("Error during scraping:", error);
-  });
-  await Webkit().catch((error) => {
-    console.error("Error during scraping:", error);
-  });
-})();
+Chromium().catch((error) => {
+  console.error("Error during scraping:", error);
+});
