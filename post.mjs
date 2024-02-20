5c52bcc0a8d55c8c500f03266cd6eca48145e1d1
shovit dutta <shovitdutta@proton.me>
2024-02-20 22:02:45 +0530

    Add temporary.txt (1)

--- post.mjs
+++ post.mjs
@@ -4,16 +4,17 @@ import fs from "fs";
 let count = 0;
 
 export async function createFile() {
-  const content = `This is the temporary file ${count}.\n`;
+  const content = "This is the temporary file " + count + ".\n";
   fs.writeFileSync("temporary.txt", content);
 }
 
 export async function commitAndPush(message) {
   return new Promise((resolve, reject) => {
-    const cmd = `git add . && git commit -m "${message}" && git push origin main`;
+    const cmd =
+      'git add . && git commit -m "' + message + '" && git push origin main';
     exec(cmd, (error, stdout, stderr) => {
       if (error || stderr) {
-        console.log(`Error: ${stderr}`);
+        console.log("Error: " + stderr);
         reject();
       } else {
         resolve();
@@ -26,9 +27,9 @@ export async function runLoop() {
   while (true) {
     try {
       await createFile();
-      await commitAndPush(`Add temporary.txt (${++count})`);
+      await commitAndPush("Add temporary.txt (" + ++count + ")");
       fs.unlinkSync("temporary.txt");
-      await commitAndPush(`Remove temporary.txt (${count})`);
+      await commitAndPush("Remove temporary.txt (" + count + ")");
       ++count;
       setTimeout(() => {}, 6000);
     } catch (e) {
