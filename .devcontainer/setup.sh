b87fc45ef1f2ce8570fce5432c99f1d7bad8dea6
Shovit Dutta <shovitdutta@proton.me>
2024-02-21 16:16:46 +0530

    yt-core + mixly

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-sudo su
-apt-get update && \
-apt-get install -y aptitude && \
-apt-get clean && \
-aptitude update && \
-aptitude safe-upgrade -y && \
-aptitude install -y dos2unix git curl wget ffmpeg opus-tools unzip nginx zsh python-is-python3 python3-pip python3-venv && \
-aptitude clean && \
-rm -rf /var/lib/apt/lists/* && \
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && \
-aptitude install -y nodejs && \
+sudo apt-get update && \
+sudo apt-get install -y aptitude && \
+sudo apt-get clean && \
+sudo aptitude update && \
+sudo aptitude safe-upgrade -y && \
+sudo aptitude install -y dos2unix git curl wget ffmpeg opus-tools unzip nginx zsh python-is-python3 python3-pip python3-venv && \
+sudo aptitude clean && \
+sudo rm -rf /var/lib/apt/lists/* && \
+sudo curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && \
+sudo aptitude install -y nodejs && \
 git clone https://github.com/shovitdutta/yt-core.git && \
 cd yt-core && \
-npm install --global yarn && \
-yarn global add playwright bun npm yt-core && \
+sudo npm install --global yarn && \
+sudo yarn global add playwright bun npm yt-core && \
 playwright install && \
 playwright install-deps && \
-yarn rebuild && \
+sudo yarn rebuild && \
 cd frontend && \
-yarn build
+sudo yarn build
