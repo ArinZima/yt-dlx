4cda1a81b6e91e3c504635e0aa9b4cc084ae0071
Shovit Dutta <shovitdutta@proton.me>
2024-02-22 00:45:23 +0530

    yt-core + mixly

--- core/base/scrape.ts
+++ core/base/scrape.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export default async function scrape(query: string): Promise<string | null> {
   try {
-    const host = "http://localhost:3000/scrape";
-    // const host = "https://yt-core-engine-shovit.koyeb.app/scrape";
+    const host = "https://yt-core-engine-shovit.koyeb.app/scrape";
     const response = await axios.get(
       host + "?query=" + encodeURIComponent(query)
     );
--- core/base/ytCore.ts
+++ core/base/ytCore.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default async function ytCore(query: string) {
-  const host = "http://localhost:3000/core";
-  // const host = "https://yt-core-engine-shovit.koyeb.app/core";
+  // const host = "http://localhost:3000/core";
+  const host = "https://yt-core-engine-shovit.koyeb.app/core";
   try {
     const response = await axios.get(
       host + "?query=" + encodeURIComponent(query)
