19399ef207146c0a4ec69aa1a5c4cbdd6ce94dca
Shovit Dutta <shovitdutta@proton.me>
2024-02-22 00:04:57 +0530

    yt-core + mixly

--- core/quick.spec.ts
+++ core/quick.spec.ts
@@ -3,14 +3,6 @@ import ytCore from ".";
 
 (async () => {
   try {
-    await ytCore.audio.playlist.custom({
-      playlistUrls: [
-        "https://youtube.com/playlist?list=PL2vrmw2gup2Jre1MK2FL72rQkzbQzFnFM&si=9U7vYacjbIfSOKr3",
-      ],
-      folderName: "temp",
-      quality: "medium",
-      verbose: false,
-    });
     await ytCore.audio.playlist.highest({
       playlistUrls: [
         "https://youtube.com/playlist?list=PL2vrmw2gup2Jre1MK2FL72rQkzbQzFnFM&si=9U7vYacjbIfSOKr3",
@@ -25,15 +17,15 @@ import ytCore from ".";
       folderName: "temp",
       verbose: false,
     });
-
-    await ytCore.video.playlist.custom({
+    await ytCore.audio.playlist.custom({
       playlistUrls: [
         "https://youtube.com/playlist?list=PL2vrmw2gup2Jre1MK2FL72rQkzbQzFnFM&si=9U7vYacjbIfSOKr3",
       ],
       folderName: "temp",
-      quality: "720p",
+      quality: "medium",
       verbose: false,
     });
+
     await ytCore.video.playlist.highest({
       playlistUrls: [
         "https://youtube.com/playlist?list=PL2vrmw2gup2Jre1MK2FL72rQkzbQzFnFM&si=9U7vYacjbIfSOKr3",
@@ -46,6 +38,14 @@ import ytCore from ".";
         "https://youtube.com/playlist?list=PL2vrmw2gup2Jre1MK2FL72rQkzbQzFnFM&si=9U7vYacjbIfSOKr3",
       ],
       folderName: "temp",
+      verbose: false,
+    });
+    await ytCore.video.playlist.custom({
+      playlistUrls: [
+        "https://youtube.com/playlist?list=PL2vrmw2gup2Jre1MK2FL72rQkzbQzFnFM&si=9U7vYacjbIfSOKr3",
+      ],
+      folderName: "temp",
+      quality: "720p",
       verbose: false,
     });
   } catch (error) {
