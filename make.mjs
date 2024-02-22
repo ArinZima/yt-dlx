73543164835f8a76d08ab75bf954472a081a34b9
Shovit Dutta <shovitdutta@proton.me>
2024-02-22 18:01:48 +0530

    yt-dlp + mixly

---
+++ make.mjs
@@ -0,0 +1,70 @@
+import readline from "readline";
+import { execSync } from "child_process";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(
+  "What operation do you want to perform? (clean/build/make/update/remake/setup/test/spec/cli/upload)\n",
+  (answer) => {
+    switch (answer.trim()) {
+      case "clean":
+        console.clear();
+        console.log("Performing clean operation...");
+        execSync("yarn run clean", { stdio: "inherit" });
+        break;
+      case "build":
+        console.clear();
+        console.log("Performing build operation...");
+        execSync("yarn run build", { stdio: "inherit" });
+        break;
+      case "make":
+        console.clear();
+        console.log("Performing make operation...");
+        execSync("yarn run make", { stdio: "inherit" });
+        break;
+      case "update":
+        console.clear();
+        console.log("Performing update operation...");
+        execSync("yarn run update", { stdio: "inherit" });
+        break;
+      case "remake":
+        console.clear();
+        console.log("Performing remake operation...");
+        execSync("yarn run remake", { stdio: "inherit" });
+        break;
+      case "setup":
+        console.clear();
+        console.log("Performing setup operation...");
+        execSync("yarn run setup", { stdio: "inherit" });
+        break;
+      case "test":
+        console.clear();
+        console.log("Performing test operation...");
+        execSync("yarn run test", { stdio: "inherit" });
+        break;
+      case "spec":
+        console.clear();
+        console.log("Performing spec operation...");
+        execSync("yarn run spec", { stdio: "inherit" });
+        break;
+      case "cli":
+        console.clear();
+        console.log("Performing cli operation...");
+        execSync("yarn run cli", { stdio: "inherit" });
+        break;
+      case "upload":
+        console.clear();
+        console.log("Performing upload operation...");
+        execSync("yarn run upload", { stdio: "inherit" });
+        break;
+      default:
+        console.clear();
+        console.log("Invalid operation selected.");
+    }
+
+    rl.close();
+  }
+);
