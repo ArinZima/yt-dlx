ec7abe8ddaffb497f67870ca1f0fb205bf11065d
Shovit Dutta <shovitdutta@proton.me>
2024-02-22 14:31:10 +0530

    yt-core + mixly

--- core/base/scrape.ts
+++ core/base/scrape.ts
@@ -7,6 +7,7 @@ export default async function scrape(query: string): Promise<string | null> {
       route: "scrape",
       domain: "https://casual-insect-sunny.ngrok-free.app",
     });
+    console.log(JSON.parse(decodeURIComponent(response as string)));
     if (response !== null) return JSON.parse(decodeURIComponent(response));
     else return null;
   } catch (error) {
