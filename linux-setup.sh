71721bc6570ac8cf340559c61859ca4f23b694cb
Shovit Dutta <shovitdutta@proton.me>
2024-02-22 22:12:27 +0530

    yt-dlp + mixly

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-sudo apt-get update && \
-sudo apt-get install -y aptitude && \
-sudo apt-get clean && \
-sudo aptitude update && \
-sudo aptitude safe-upgrade -y && \
-sudo aptitude install -y dos2unix git curl wget ffmpeg opus-tools unzip nginx python-is-python3 python3-pip python3-venv && \
-sudo aptitude clean && \
-sudo rm -rf /var/lib/apt/lists/* && \
-sudo curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && \
-sudo aptitude install -y nodejs && \
-sudo npm install --global yarn bun && \
-sudo yarn global add playwright npm yt-dlp tsup ts-node typescript && \
-playwright install && \
-playwright install-deps && \
-yarn remake
+log_file="/var/log/script.log"
+log_error() {
+    local message="$1"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - ERROR: $message" >> "$log_file"
+}
+log_info() {
+    local message="$1"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - INFO: $message" >> "$log_file"
+}
+run_command() {
+    local command="$1"
+    local error_message="$2"
+    if ! eval "$command"; then
+        log_error "$error_message"
+    fi
+}
+
+run_command "sudo apt-get update" "Failed to update APT repositories"
+run_command "sudo apt-get install -y aptitude" "Failed to install aptitude"
+run_command "sudo apt-get clean" "Failed to clean APT cache"
+run_command "sudo aptitude update" "Failed to update aptitude"
+run_command "sudo aptitude safe-upgrade -y" "Failed to perform safe-upgrade with aptitude"
+run_command "sudo aptitude install -y dos2unix git curl wget ffmpeg opus-tools unzip nginx python-is-python3 python3-pip python3-venv" "Failed to install packages with aptitude"
+run_command "sudo aptitude clean" "Failed to clean aptitude cache"
+run_command "sudo rm -rf /var/lib/apt/lists/*" "Failed to remove APT lists"
+run_command "sudo curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -" "Failed to setup Node.js repository"
+run_command "sudo aptitude install -y nodejs" "Failed to install Node.js"
+run_command "sudo npm install --global yarn bun" "Failed to install global npm packages"
+run_command "sudo yarn global add playwright npm yt-dlp tsup ts-node typescript" "Failed to install global yarn packages"
+run_command "playwright install" "Failed to install Playwright dependencies"
+run_command "playwright install-deps" "Failed to install Playwright dependencies"
+run_command "yarn remake" "Failed to run yarn remake"
+log_info "Script execution completed"
