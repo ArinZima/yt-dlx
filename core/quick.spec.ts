7396bebf3d809d9d0a10f9bb7c5eef05ad9b7f97
Shovit Dutta <shovitdutta@proton.me>
2024-02-22 14:49:29 +0530

    yt-core + mixly

--- core/quick.spec.ts
+++ core/quick.spec.ts
@@ -1,111 +1,99 @@
 console.clear();
 import ytCore from ".";
-// import * as async from "async";
-// import ListAudioLowest from "./pipes/audio/ListAudioLowest";
-// import ListAudioHighest from "./pipes/audio/ListAudioHighest";
-// import ListVideoLowest from "./pipes/video/ListVideoLowest";
-// import ListVideoHighest from "./pipes/video/ListVideoHighest";
-// import ListAudioQualityCustom from "./pipes/audio/ListAudioQualityCustom";
-// import ListVideoQualityCustom from "./pipes/video/ListVideoQualityCustom";
-// const playlistUrls: string[] = [
-// "https://youtube.com/playlist?list=PL2vrmw2gup2Jre1MK2FL72rQkzbQzFnFM&si=9U7vYacjbIfSOKr3",
-// ];
+import * as async from "async";
+import ListAudioLowest from "./pipes/audio/ListAudioLowest";
+import ListAudioHighest from "./pipes/audio/ListAudioHighest";
+import ListVideoLowest from "./pipes/video/ListVideoLowest";
+import ListVideoHighest from "./pipes/video/ListVideoHighest";
+import ListAudioQualityCustom from "./pipes/audio/ListAudioQualityCustom";
+import ListVideoQualityCustom from "./pipes/video/ListVideoQualityCustom";
+const playlistUrls: string[] = [
+  "https://youtube.com/playlist?list=PL2vrmw2gup2Jre1MK2FL72rQkzbQzFnFM&si=9U7vYacjbIfSOKr3",
+];
 
-(async () => {
-  try {
-    await ytCore.audio.single.lowest({
-      query: "wWR0VD6qgt8",
-      folderName: "temp",
-      verbose: false,
-    });
-  } catch (error) {
-    console.error(error);
+async.waterfall(
+  [
+    async (callback: (arg0: unknown) => void) => {
+      try {
+        await ytCore.audio.playlist.custom({
+          folderName: "temp",
+          quality: "medium",
+          verbose: false,
+          playlistUrls,
+        });
+        await ytCore.audio.playlist.highest({
+          folderName: "temp",
+          verbose: false,
+          playlistUrls,
+        });
+        await ytCore.audio.playlist.lowest({
+          folderName: "temp",
+          verbose: false,
+          playlistUrls,
+        });
+
+        await ytCore.video.playlist.custom({
+          folderName: "temp",
+          quality: "720p",
+          verbose: false,
+          playlistUrls,
+        });
+        await ytCore.video.playlist.highest({
+          folderName: "temp",
+          verbose: false,
+          playlistUrls,
+        });
+        await ytCore.video.playlist.lowest({
+          folderName: "temp",
+          verbose: false,
+          playlistUrls,
+        });
+        callback(null);
+      } catch (error) {
+        callback(error);
+      }
+    },
+    async (callback: (arg0: unknown) => void) => {
+      try {
+        await ListAudioLowest({
+          folderName: "temp",
+          verbose: false,
+          playlistUrls,
+        });
+        await ListAudioHighest({
+          folderName: "temp",
+          verbose: false,
+          playlistUrls,
+        });
+        await ListAudioQualityCustom({
+          folderName: "temp",
+          quality: "medium",
+          verbose: false,
+          playlistUrls,
+        });
+        await ListVideoLowest({
+          folderName: "temp",
+          verbose: false,
+          playlistUrls,
+        });
+        await ListVideoHighest({
+          folderName: "temp",
+          verbose: false,
+          playlistUrls,
+        });
+        await ListVideoQualityCustom({
+          folderName: "temp",
+          quality: "720p",
+          verbose: false,
+          playlistUrls,
+        });
+        callback(null);
+      } catch (error) {
+        callback(error);
+      }
+    },
+  ],
+  (err) => {
+    if (err) console.error(err);
   }
-})();
-
-// async.waterfall(
-// [
-// async (callback: any) => {
-// try {
-// await ytCore.audio.playlist.custom({
-// folderName: "temp",
-// quality: "medium",
-// verbose: false,
-// playlistUrls,
-// });
-// await ytCore.audio.playlist.highest({
-// folderName: "temp",
-// verbose: false,
-// playlistUrls,
-// });
-// await ytCore.audio.playlist.lowest({
-// folderName: "temp",
-// verbose: false,
-// playlistUrls,
-// });
-
-// await ytCore.video.playlist.custom({
-// folderName: "temp",
-// quality: "720p",
-// verbose: false,
-// playlistUrls,
-// });
-// await ytCore.video.playlist.highest({
-// folderName: "temp",
-// verbose: false,
-// playlistUrls,
-// });
-// await ytCore.video.playlist.lowest({
-// folderName: "temp",
-// verbose: false,
-// playlistUrls,
-// });
-// callback(null);
-// } catch (error) {
-// callback(error);
-// }
-// },
-// async (callback: any) => {
-// try {
-// await ListAudioLowest({
-// folderName: "temp",
-// verbose: false,
-// playlistUrls,
-// });
-// await ListAudioHighest({
-// folderName: "temp",
-// verbose: false,
-// playlistUrls,
-// });
-// await ListAudioQualityCustom({
-// folderName: "temp",
-// quality: "medium",
-// verbose: false,
-// playlistUrls,
-// });
-// await ListVideoLowest({
-// folderName: "temp",
-// verbose: false,
-// playlistUrls,
-// });
-// await ListVideoHighest({
-// folderName: "temp",
-// verbose: false,
-// playlistUrls,
-// });
-// await ListVideoQualityCustom({
-// folderName: "temp",
-// quality: "720p",
-// verbose: false,
-// playlistUrls,
-// });
-// callback(null);
-// } catch (error) {
-// callback(error);
-// }
-// },
-// ],
-// (err) => {
-// if (err) console.error(err);
-// }
-// );
+);
