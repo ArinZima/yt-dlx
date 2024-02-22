a28fce3c21c70634eb1bff64df3f66dcdaaad0b2
Shovit Dutta <shovitdutta@proton.me>
2024-02-22 14:52:08 +0530

    yt-core + mixly

--- core/quick.spec.ts
+++ core/quick.spec.ts
@@ -1,6 +1,5 @@
 console.clear();
 import ytCore from ".";
-import * as async from "async";
 import ListAudioLowest from "./pipes/audio/ListAudioLowest";
 import ListAudioHighest from "./pipes/audio/ListAudioHighest";
 import ListVideoLowest from "./pipes/video/ListVideoLowest";
@@ -9,64 +8,50 @@ const playlistUrls: string[] = [
   "https://youtube.com/playlist?list=PL2vrmw2gup2Jre1MK2FL72rQkzbQzFnFM&si=9U7vYacjbIfSOKr3",
 ];
 
-async.waterfall(
-  [
-    async (callback: (arg0: unknown) => void) => {
-      try {
-        await ytCore.audio.playlist.highest({
-          folderName: "temp",
-          verbose: false,
-          playlistUrls,
-        });
-        await ytCore.audio.playlist.lowest({
-          folderName: "temp",
-          verbose: false,
-          playlistUrls,
-        });
-        await ytCore.video.playlist.highest({
-          folderName: "temp",
-          verbose: false,
-          playlistUrls,
-        });
-        await ytCore.video.playlist.lowest({
-          folderName: "temp",
-          verbose: false,
-          playlistUrls,
-        });
-        callback(null);
-      } catch (error) {
-        callback(error);
-      }
-    },
-    async (callback: (arg0: unknown) => void) => {
-      try {
-        await ListAudioLowest({
-          folderName: "temp",
-          verbose: false,
-          playlistUrls,
-        });
-        await ListAudioHighest({
-          folderName: "temp",
-          verbose: false,
-          playlistUrls,
-        });
-        await ListVideoLowest({
-          folderName: "temp",
-          verbose: false,
-          playlistUrls,
-        });
-        await ListVideoHighest({
-          folderName: "temp",
-          verbose: false,
-          playlistUrls,
-        });
-        callback(null);
-      } catch (error) {
-        callback(error);
-      }
-    },
-  ],
-  (err) => {
-    if (err) console.error(err);
+(async () => {
+  try {
+    await ytCore.audio.playlist.highest({
+      folderName: "temp",
+      verbose: false,
+      playlistUrls,
+    });
+    await ytCore.audio.playlist.lowest({
+      folderName: "temp",
+      verbose: false,
+      playlistUrls,
+    });
+    await ytCore.video.playlist.highest({
+      folderName: "temp",
+      verbose: false,
+      playlistUrls,
+    });
+    await ytCore.video.playlist.lowest({
+      folderName: "temp",
+      verbose: false,
+      playlistUrls,
+    });
+
+    await ListAudioLowest({
+      folderName: "temp",
+      verbose: false,
+      playlistUrls,
+    });
+    await ListAudioHighest({
+      folderName: "temp",
+      verbose: false,
+      playlistUrls,
+    });
+    await ListVideoLowest({
+      folderName: "temp",
+      verbose: false,
+      playlistUrls,
+    });
+    await ListVideoHighest({
+      folderName: "temp",
+      verbose: false,
+      playlistUrls,
+    });
+  } catch (error) {
+    console.error(error);
   }
-);
+})();
