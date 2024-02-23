319bb5e7e01b9520fd80adda9b388e88a09d8ee9
Shovit Dutta <shovitdutta@proton.me>
2024-02-24 02:51:25 +0530

    ytdlx + mixly

--- main.mjs
+++ main.mjs
@@ -1,145 +1,182 @@
 console.clear();
+import async from "async";
 import colors from "colors";
+import retry from "async-retry";
 import { chromium } from "playwright";
 
 async function YouTubeSearch(query) {
-  let videos = [];
-  const data = [];
-  console.log(colors.yellow("@scrape:"), "spinning chromium...");
-  const browser = await chromium.launch({ headless: true });
-  const context = await browser.newContext({ ignoreHTTPSErrors: true });
-  const page = await context.newPage();
-  const searchUrl =
-    "https://www.youtube.com/results?search_query=" + encodeURIComponent(query);
-  await page.goto(searchUrl);
-  console.log(colors.yellow("@scrape:"), "waiting for hydration...");
-  while (videos.length < 40) {
-    await page.waitForSelector(".ytd-video-renderer");
-    const newVideos = await page.$$(
-      'ytd-video-renderer:not([class*="ytd-rich-grid-video-renderer"])'
-    );
-    videos = [...videos, ...newVideos];
-    await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
-  }
-  for (const vid of videos) {
-    const title = await vid.$eval("#video-title", (el) =>
-      el.textContent.trim()
-    );
-    const videoLink =
-      "https://www.youtube.com" +
-      (await vid.$eval("a", (el) => el.getAttribute("href")));
-    const videoId = videoLink.match(
-      /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?.*v=([^&]+)/
-    )[1];
-    const authorContainer = await vid.$(".ytd-channel-name a");
-    const author = await authorContainer
-      .getProperty("textContent")
-      .then((property) => property.jsonValue());
-    const authorUrl = await authorContainer
-      .getProperty("href")
-      .then((property) => property.jsonValue());
-    let description = "";
-    const descriptionElement = await vid.$(".metadata-snippet-text");
-    if (descriptionElement) {
-      description = await descriptionElement
-        .getProperty("innerText")
-        .then((property) => property.jsonValue());
-    }
-    const viewsContainer = await vid.$(
-      ".inline-metadata-item.style-scope.ytd-video-meta-block"
-    );
-    const views = await viewsContainer
-      .getProperty("innerText")
-      .then((property) => property.jsonValue());
-    data.push({
-      title,
-      author,
-      videoId,
-      authorUrl,
-      videoLink,
-      thumbnailUrl:
-        "https://img.youtube.com/vi/" + videoId + "/maxresdefault.jpg",
-      description,
-      views: views.replace(/ views/g, ""),
-    });
-  }
-  await browser.close();
-  console.log(
-    colors.green("browser @scrape:"),
-    "found total videos:",
-    videos.length
-  );
-  return data;
-}
-
-async function YouTubeSingle(videoUrl) {
-  console.log(colors.yellow("@scrape:"), "spinning chromium...");
-  const browser = await chromium.launch({ headless: true });
-  const context = await browser.newContext({ ignoreHTTPSErrors: true });
-  const page = await context.newPage();
+  const retryOptions = {
+    retries: 3,
+    minTimeout: 1000,
+    maxTimeout: 3000,
+  };
   try {
-    await page.goto(videoUrl);
-    console.log(colors.yellow("@scrape:"), "waiting for hydration...");
-    await page.waitForSelector(".style-scope.ytd-watch-metadata");
-    const title = await page.$eval(".style-scope.ytd-watch-metadata", (el) =>
-      el.textContent.trim()
-    );
-    const author = await page.$eval(
-      ".yt-simple-endpoint.style-scope.yt-formatted-string",
-      (el) => el.textContent.trim()
-    );
-    const views = await page.$eval(
-      ".bold.style-scope.yt-formatted-string",
-      (el) => el.textContent.trim()
-    );
-    const videoId = videoUrl.match(
-      /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?.*v=([^&]+)/
-    )[1];
-    const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
-    await page.waitForSelector(".item.style-scope.ytd-watch-metadata");
-    const description = await page.$eval(
-      ".item.style-scope.ytd-watch-metadata",
-      (el) => el.textContent.trim()
-    );
-    const uploadOn = await page.$eval(
-      "span.bold.style-scope.yt-formatted-string",
-      (el) => el.textContent.trim()
-    );
-    const data = {
-      title: title.split("\n")[0].trim(),
-      author,
-      videoId,
-      videoUrl,
-      thumbnailUrl,
-      views: views.replace(/ views/g, ""),
-      description,
-      uploadOn,
-    };
-    console.log(
-      colors.green("browser @scrape:"),
-      "Video data retrieved successfully."
-    );
-    return data;
+    const searchData = await retry(async () => {
+      let videos = [];
+      const data = [];
+      const browser = await chromium.launch({ headless: true });
+      console.log(colors.yellow("@scrape:"), "spinning chromium...");
+      const context = await browser.newContext({
+        ignoreHTTPSErrors: true,
+        serviceWorkers: "allow",
+        bypassCSP: true,
+        userAgent:
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36",
+      });
+      const page = await context.newPage();
+      const searchUrl =
+        "https://www.youtube.com/results?search_query=" +
+        encodeURIComponent(query);
+      await page.goto(searchUrl);
+      console.log(colors.yellow("@scrape:"), "waiting for hydration...");
+      while (videos.length < 40) {
+        await page.waitForSelector(".ytd-video-renderer");
+        const newVideos = await page.$$(
+          'ytd-video-renderer:not([class*="ytd-rich-grid-video-renderer"])'
+        );
+        videos = [...videos, ...newVideos];
+        await page.evaluate(() =>
+          window.scrollTo(0, document.body.scrollHeight)
+        );
+      }
+      for (const vid of videos) {
+        const title = await vid.$eval("#video-title", (el) =>
+          el.textContent.trim()
+        );
+        const videoLink =
+          "https://www.youtube.com" +
+          (await vid.$eval("a", (el) => el.getAttribute("href")));
+        const videoId = videoLink.match(
+          /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?.*v=([^&]+)/
+        )[1];
+        const authorContainer = await vid.$(".ytd-channel-name a");
+        const author = await authorContainer
+          .getProperty("textContent")
+          .then((property) => property.jsonValue());
+        const authorUrl = await authorContainer
+          .getProperty("href")
+          .then((property) => property.jsonValue());
+        let description = "";
+        const descriptionElement = await vid.$(".metadata-snippet-text");
+        if (descriptionElement) {
+          description = await descriptionElement
+            .getProperty("innerText")
+            .then((property) => property.jsonValue());
+        }
+        const viewsContainer = await vid.$(
+          ".inline-metadata-item.style-scope.ytd-video-meta-block"
+        );
+        const views = await viewsContainer
+          .getProperty("innerText")
+          .then((property) => property.jsonValue());
+        data.push({
+          title,
+          author,
+          videoId,
+          authorUrl,
+          videoLink,
+          thumbnailUrl:
+            "https://img.youtube.com/vi/" + videoId + "/maxresdefault.jpg",
+          description,
+          views: views.replace(/ views/g, ""),
+        });
+      }
+      await browser.close();
+      return data;
+    }, retryOptions);
+    return searchData;
   } catch (error) {
-    console.error(error);
+    console.error(
+      colors.red("@error:"),
+      `Failed to search YouTube: ${error.message}`
+    );
     return null;
-  } finally {
-    await browser.close();
   }
 }
 
-(async () => {
-  await YouTubeSearch("ZULFAAN (Official Audio) SARRB | Starboy X")
-    .then((data) => {
-      if (data) console.log(data);
-      else console.log("Failed to retrieve video data.");
-    })
-    .catch((error) => console.error(error));
+async function YouTubeVideo(videoUrl) {
+  const retryOptions = {
+    retries: 3,
+    minTimeout: 1000,
+    maxTimeout: 3000,
+  };
+  try {
+    const videoData = await retry(async () => {
+      const browser = await chromium.launch({ headless: true });
+      console.log(colors.yellow("@scrape:"), "spinning chromium...");
+      const context = await browser.newContext({
+        ignoreHTTPSErrors: true,
+        serviceWorkers: "allow",
+        bypassCSP: true,
+        userAgent:
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36",
+      });
+      const page = await context.newPage();
+      await page.goto(videoUrl);
+      console.log(colors.yellow("@scrape:"), "waiting for hydration...");
+      await page.waitForSelector(".style-scope.ytd-watch-metadata");
+      const title = await page.$eval(".style-scope.ytd-watch-metadata", (el) =>
+        el.textContent.trim()
+      );
+      const author = await page.$eval(
+        ".yt-simple-endpoint.style-scope.yt-formatted-string",
+        (el) => el.textContent.trim()
+      );
+      const views = await page.$eval(
+        ".bold.style-scope.yt-formatted-string",
+        (el) => el.textContent.trim()
+      );
+      const videoId = videoUrl.match(
+        /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?.*v=([^&]+)/
+      )[1];
+      const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+      const uploadDateElements = await page.$$eval(
+        ".bold.style-scope.yt-formatted-string",
+        (spans) => {
+          const uploadDateIndex = spans.findIndex((span) =>
+            span.textContent.includes("ago")
+          );
+          return uploadDateIndex >= 0
+            ? spans[uploadDateIndex].textContent.trim()
+            : null;
+        }
+      );
+      const uploadOn = uploadDateElements;
+      const data = {
+        title: title.split("\n")[0].trim(),
+        author,
+        videoId,
+        videoUrl,
+        thumbnailUrl,
+        views: views.replace(/ views/g, ""),
+        uploadOn,
+      };
+      await browser.close();
+      return data;
+    }, retryOptions);
+    return videoData;
+  } catch (error) {
+    console.error(
+      colors.red("@error:"),
+      `Failed to get video info: ${error.message}`
+    );
+    return null;
+  }
+}
 
-  await YouTubeSingle("https://www.youtube.com/watch?v=_oTgwjM6mBU")
-    .then((data) => {
-      if (data) console.log(data);
-      else console.log("Failed to retrieve video data.");
-    })
-    .catch((error) => console.error(error));
-})();
+async.waterfall(
+  [
+    async function searchYouTube() {
+      const searchData = await YouTubeSearch("Angel Numbers / Ten Toes");
+      return searchData;
+    },
+    async function getVideoInfo(searchData) {
+      const videoData = await YouTubeVideo(searchData[0].videoLink);
+      return videoData;
+    },
+  ],
+  function (error, result) {
+    if (error) console.error(colors.red("@error:"), error);
+    else console.log(colors.blue("@stdout:"), result);
+  }
+);
