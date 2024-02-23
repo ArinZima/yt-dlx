29eef81facea1965ccd95125a220e3923785f475
Shovit Dutta <shovitdutta@proton.me>
2024-02-23 12:18:24 +0530

    yt-dlp + mixly

--- stack/app/exAsync.ts
+++ stack/app/exAsync.ts
@@ -1,3 +1,4 @@
+import { join } from "path";
 import { promisify } from "util";
 import { exec } from "child_process";
 import type ExAsyncParams from "../../app/interface/ExAsyncParams";
@@ -9,9 +10,9 @@ export default async function exAsync({
 }: ExAsyncParams): Promise<string | null> {
   for (let i = 0; i < retries; i++) {
     try {
-      let proLoc: string = "python -m yt_dlp ";
-      if (proxy) proLoc += `--proxy '${proxy}' --dump-json '${query}'`;
-      else proLoc += `--dump-json '${query}'`;
+      let proLoc: string = join(__dirname, "ytDlp");
+      if (proxy) proLoc += ` --proxy '${proxy}' --dump-json '${query}'`;
+      else proLoc += ` --dump-json '${query}'`;
       const result = await promisify(exec)(proLoc);
       if (result.stderr) console.error(result.stderr.toString());
       return result.stdout.toString() || null;
