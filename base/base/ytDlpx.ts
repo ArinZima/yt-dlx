d6419d2a65a7339da7b1f16e0735849537fc63b3
Shovit Dutta <shovitdutta@proton.me>
2024-02-23 17:49:42 +0530

    yt-dlp + mixly

--- base/base/ytDlpx.ts
+++ base/base/ytDlpx.ts
@@ -14,8 +14,9 @@ export default async function ytDlpx({
 }: ytDlpx): Promise<string | null> {
   const browser: Browser = await chromium.launch({ headless: true });
   try {
-    const item: string = "query=" + decodeURIComponent(query);
-    const host: string = `${domain}/${route}?${item}`;
+    const host = `${decodeURIComponent(domain)}/${decodeURIComponent(
+      route
+    )}?query=${decodeURIComponent(query)}`;
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto(host);
@@ -29,12 +30,13 @@ export default async function ytDlpx({
           if (request.url().includes("/" + route)) {
             const response = await request.response();
             if (response) {
+              console.log(response);
               const json = await response.json();
               resolve(json);
             } else resolve(null);
           }
         } catch (error) {
-          console.log(colors.red("response @error:"), error);
+          console.log(colors.red("@error:"), error);
           resolve(null);
         }
       });
@@ -44,14 +46,11 @@ export default async function ytDlpx({
       await browser.close();
       return JSON.stringify(payLoad);
     } else {
-      console.log(colors.red("fail @query:"), query);
       await browser.close();
       return null;
     }
   } catch (error) {
     console.log(colors.red("@error:"), error);
     return null;
-  } finally {
-    await browser.close();
   }
 }
