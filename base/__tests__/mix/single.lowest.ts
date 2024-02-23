beac32e8b6ed344018d05556b552540de5a75670
Shovit Dutta <shovitdutta@proton.me>
2024-02-23 19:19:12 +0530

    yt-dlp + mixly

---
+++ base/__tests__/mix/single.lowest.ts
@@ -0,0 +1,132 @@
+import * as fs from "fs";
+import ytDlp from "../..";
+import fsx from "fs-extra";
+import colors from "colors";
+import * as chai from "chai";
+
+async function AutoDownloadTest() {
+  try {
+    let holder: any;
+    console.log(
+      colors.bold.yellow("@test:"),
+      "ytDlp.audio_video.single.lowest()"
+    );
+    console.log(colors.bold.yellow("@info:"), "stream: false");
+    holder = await ytDlp.audio_video.single.lowest({
+      query: "SuaeRys5tTc",
+      outputFormat: "mp4",
+      folderName: "video",
+      stream: false,
+    });
+    switch (true) {
+      case "status" in holder:
+        chai.expect(holder.status).to.equal(200);
+        console.log(
+          colors.bold.green("\n@pass:"),
+          `with status ${holder.status}`
+        );
+        await fsx.remove("video");
+        break;
+      default:
+        console.error("\n", colors.bold.red("\n@error:"), holder);
+        await fsx.remove("video");
+        process.exit(0);
+    }
+    console.log(
+      colors.bold.yellow("@test:"),
+      "ytDlp.audio_video.single.lowest()"
+    );
+    console.log(colors.bold.yellow("@info:"), "stream: false");
+    holder = await ytDlp.audio_video.single.lowest({
+      query: "SuaeRys5tTc",
+      outputFormat: "mov",
+      folderName: "video",
+      stream: false,
+    });
+    switch (true) {
+      case "status" in holder:
+        chai.expect(holder.status).to.equal(200);
+        console.log(
+          colors.bold.green("\n@pass:"),
+          `with status ${holder.status}`
+        );
+        await fsx.remove("video");
+        break;
+      default:
+        console.error("\n", colors.bold.red("\n@error:"), holder);
+        await fsx.remove("video");
+        process.exit(0);
+    }
+  } catch (error) {
+    console.error("\n", colors.bold.red("\n@error:"), error);
+    await fsx.remove("video");
+  }
+}
+async function StreamingTest() {
+  try {
+    let holder: any;
+    console.log(
+      colors.bold.yellow("@test:"),
+      "ytDlp.audio_video.single.lowest()"
+    );
+    console.log(colors.bold.yellow("@info:"), "stream: true");
+    holder = await ytDlp.audio_video.single.lowest({
+      query: "SuaeRys5tTc",
+      outputFormat: "avi",
+      folderName: "video",
+      stream: true,
+    });
+    switch (true) {
+      case "stream" in holder && "filename" in holder:
+        chai.expect(holder.stream && holder.filename).to.exist;
+        holder.stream.pipe(fs.createWriteStream(holder.filename));
+        console.log(
+          colors.bold.green("\n@pass:"),
+          `with filename ${holder.filename}`
+        );
+        await fsx.remove("video");
+        break;
+      default:
+        console.error("\n", colors.bold.red("\n@error:"), holder);
+        await fsx.remove("video");
+        process.exit(0);
+    }
+
+    console.log(
+      colors.bold.yellow("@test:"),
+      "ytDlp.audio_video.single.lowest()"
+    );
+    console.log(colors.bold.yellow("@info:"), "stream: true");
+    holder = await ytDlp.audio_video.single.lowest({
+      query: "SuaeRys5tTc",
+      outputFormat: "mp4",
+      folderName: "video",
+      stream: true,
+    });
+    switch (true) {
+      case "stream" in holder && "filename" in holder:
+        chai.expect(holder.stream && holder.filename).to.exist;
+        holder.stream.pipe(fs.createWriteStream(holder.filename));
+        console.log(
+          colors.bold.green("\n@pass:"),
+          `with filename ${holder.filename}`
+        );
+        await fsx.remove("video");
+        break;
+      default:
+        console.error("\n", colors.bold.red("\n@error:"), holder);
+        await fsx.remove("video");
+        process.exit(0);
+    }
+  } catch (error) {
+    console.error("\n", colors.bold.red("\n@error:"), error);
+    await fsx.remove("video");
+  }
+}
+
+(async () => {
+  console.log(colors.bold.blue("\n@test type:"), "AutoDownloadTest()");
+  await AutoDownloadTest();
+  console.log(colors.bold.blue("\n@test type:"), "StreamingTest()");
+  await StreamingTest();
+})();
