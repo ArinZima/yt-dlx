6c7e9db6693f04882aa65c94af24584582ea3675
Shovit Dutta <shovitdutta@proton.me>
2024-02-23 13:16:08 +0530

    yt-dlp + mixly

--- core/app/base/ytDlpx.d.ts
+++
@@ -1,8 +0,0 @@
-interface ytDlpx {
-    domain: string;
-    query: string;
-    route: string;
-}
-export default function ytDlpx({ query, route, domain, }: ytDlpx): Promise<string | null>;
-export {};
-//# sourceMappingURL=ytDlpx.d.ts.map
--- stack/core/stack/app/scrape.spec.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=scrape.spec.d.ts.map
