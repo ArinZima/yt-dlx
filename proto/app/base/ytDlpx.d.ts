e9630c2a0a3e63602c08229bf4fc2ca700be0801
Shovit Dutta <shovitdutta@proton.me>
2024-02-23 15:02:33 +0530

    yt-dlp + mixly

---
+++ proto/app/base/ytDlpx.d.ts
@@ -0,0 +1,8 @@
+interface ytDlpx {
+    domain: string;
+    query: string;
+    route: string;
+}
+export default function ytDlpx({ query, route, domain, }: ytDlpx): Promise<string | null>;
+export {};
+//# sourceMappingURL=ytDlpx.d.ts.map
