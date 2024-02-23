f3a2717bd1723efb8e52ed2703165ea528af33bc
Shovit Dutta <shovitdutta@proton.me>
2024-02-23 12:40:49 +0530

    yt-dlp + mixly

--- stack/app/ytDlpx.spec.ts
+++ stack/app/ytDlpx.spec.ts
@@ -34,62 +34,75 @@ async function ytDlpx({ query, route, domain }: any) {
     const payLoad = await payLoadPromise;
     if (payLoad) {
       console.log(colors.green("pass @url:"), host);
+      await browser.close();
       return payLoad;
     } else {
       console.log(colors.red("fail @url:"), host);
+      await browser.close();
       return null;
     }
   } catch (error) {
     console.log(colors.red("ERROR:"), error);
     return null;
-  } finally {
-    await browser.close();
   }
 }
 
 async function runTests() {
+  let metaTube;
   try {
-    await ytDlpx({
+    metaTube = await ytDlpx({
       route: "core",
       query: "wWR0VD6qgt8",
       domain: "https://possible-willingly-yeti.ngrok-free.app",
     });
+    if (metaTube === null) process.exit(0);
+    else console.log(metaTube);
     await delay(2000);
 
-    await ytDlpx({
+    metaTube = await ytDlpx({
       route: "scrape",
       query: "angel numbers",
       domain: "https://possible-willingly-yeti.ngrok-free.app",
     });
+    if (metaTube === null) process.exit(0);
+    else console.log(metaTube);
     await delay(2000);
 
-    await ytDlpx({
+    metaTube = await ytDlpx({
       route: "scrape",
       query: "wWR0VD6qgt8",
       domain: "https://possible-willingly-yeti.ngrok-free.app",
     });
+    if (metaTube === null) process.exit(0);
+    else console.log(metaTube);
     await delay(2000);
 
-    await ytDlpx({
+    metaTube = await ytDlpx({
       route: "scrape",
       domain: "https://possible-willingly-yeti.ngrok-free.app",
       query: "https://youtu.be/wWR0VD6qgt8?si=S8os0alEDZ6875lD",
     });
+    if (metaTube === null) process.exit(0);
+    else console.log(metaTube);
     await delay(2000);
 
-    await ytDlpx({
+    metaTube = await ytDlpx({
       route: "scrape",
       query: "PL2vrmw2gup2Jre1MK2FL72rQkzbQzFnFM",
       domain: "https://possible-willingly-yeti.ngrok-free.app",
     });
+    if (metaTube === null) process.exit(0);
+    else console.log(metaTube);
     await delay(2000);
 
-    await ytDlpx({
+    metaTube = await ytDlpx({
       route: "scrape",
       domain: "https://possible-willingly-yeti.ngrok-free.app",
       query:
         "https://youtube.com/playlist?list=PL2vrmw2gup2Jre1MK2FL72rQkzbQzFnFM&si=RW12dM2je3XvbH2g",
     });
+    if (metaTube === null) process.exit(0);
+    else console.log(metaTube);
     await delay(2000);
 
     process.exit(0);
