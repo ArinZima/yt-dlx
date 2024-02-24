eee38e4a611259b117acac04ad1e65d99166be6d
Shovit Dutta <shovitdutta@proton.me>
2024-02-24 22:53:54 +0530

    yt-dlx @alpha

--- rproxy.mjs
+++ rproxy.mjs
@@ -138,5 +138,21 @@ const reops = {
     console.info(colors.green("@info:"), "data saved in", filePath);
   } catch (error) {
     console.error(colors.red("@error:"), error.message);
+    const __filename = fileURLToPath(import.meta.url);
+    let proLoc =
+      join(dirname(__filename), "backend", "util", "Engine") +
+      " " +
+      "--dump-single-json --no-check-certificate --prefer-insecure --no-call-home --skip-download --no-warnings --geo-bypass " +
+      "--user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36' " +
+      "'https://www.youtube.com/watch?v=wWR0VD6qgt8'";
+    const result = await retry(async (bail) => {
+      const proc = await promisify(exec)(proLoc);
+      if (proc.stderr) bail(new Error(proc.stderr.toString()));
+      return proc.stdout;
+    }, reops);
+    const jsonData = JSON.parse(result.toString());
+    const filePath = join(dirname(__filename), "Engine.json");
+    fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+    console.info(colors.green("@info:"), "data saved in", filePath);
   }
 })();
