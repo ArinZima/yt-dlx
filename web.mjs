f6a72fc1bb82df9949e387a2484e6e04296a6a8b
yt-dlx <yt-dlx@proton.me>
2024-02-26 02:34:57 -0500

    yt-dlx @2.0.0

--- web.mjs
+++ web.mjs
@@ -2,31 +2,7 @@ import fs from "fs";
 import async from "async";
 import puppeteer from "puppeteer";
 
-async function TypeTube(query, type = "FilterTypeSearch") {
-  let fileName = "";
-  let urlSuffix = "";
-  switch (type) {
-    case "FilterTypeVideo":
-      urlSuffix = "EgIIAQ%253D%253D";
-      fileName = "FilterTypeVideo.png";
-      break;
-    case "FilterTypeChannel":
-      urlSuffix = "EgIQAg%253D%253D";
-      fileName = "FilterTypeChannel.png";
-      break;
-    case "FilterTypePlaylist":
-      urlSuffix = "EgIQAw%253D%253D";
-      fileName = "FilterTypePlaylist.png";
-      break;
-    case "FilterTypeMovie":
-      urlSuffix = "EgIQBA%253D%253D";
-      fileName = "FilterTypeMovie.png";
-      break;
-    default:
-      urlSuffix = "";
-      fileName = "FilterTypeSearch.png";
-      break;
-  }
+async function TypeTube(query, type = "TypeSearch") {
   const browser = await puppeteer.launch({
     userDataDir: "other",
     headless: true,
@@ -35,26 +11,77 @@ async function TypeTube(query, type = "FilterTypeSearch") {
   await page.setUserAgent(
     "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
   );
-  const url =
-    "https://www.youtube.com/results?search_query=" +
-    decodeURIComponent(query) +
-    "&sp=" +
-    urlSuffix;
-  await page.goto(url);
-  const screenshot = await page.screenshot({
-    path: fileName,
-  });
-  fs.writeFileSync(fileName, screenshot);
-  await page.close();
-  await browser.close();
+  let url;
+  let screenshot;
+  switch (type) {
+    case "TypeVideo":
+      url =
+        "https://www.youtube.com/results?search_query=" +
+        decodeURIComponent(query) +
+        "&sp=EgIIAQ%253D%253D";
+      await page.goto(url);
+      screenshot = await page.screenshot({
+        path: "TypeVideo.png",
+      });
+      fs.writeFileSync("TypeVideo.png", screenshot);
+      await page.close();
+      await browser.close();
+      break;
+    case "TypeChannel":
+      url =
+        "https://www.youtube.com/results?search_query=" +
+        decodeURIComponent(query) +
+        "&sp=EgIQAg%253D%253D";
+      await page.goto(url);
+      screenshot = await page.screenshot({
+        path: "TypeChannel.png",
+      });
+      fs.writeFileSync("TypeChannel.png", screenshot);
+      await page.close();
+      await browser.close();
+      break;
+    case "TypePlaylist":
+      url =
+        "https://www.youtube.com/results?search_query=" +
+        decodeURIComponent(query) +
+        "&sp=EgIQAw%253D%253D";
+      await page.goto(url);
+      screenshot = await page.screenshot({
+        path: "TypePlaylist.png",
+      });
+      fs.writeFileSync("TypePlaylist.png", screenshot);
+      await page.close();
+      await browser.close();
+      break;
+    case "TypeMovie":
+      url =
+        "https://www.youtube.com/results?search_query=" +
+        decodeURIComponent(query) +
+        "&sp=EgIQBA%253D%253D";
+      await page.goto(url);
+      screenshot = await page.screenshot({
+        path: "TypeMovie.png",
+      });
+      fs.writeFileSync("TypeMovie.png", screenshot);
+      await page.close();
+      await browser.close();
+      break;
+    default:
+      url =
+        "https://www.youtube.com/results?search_query=" +
+        decodeURIComponent(query);
+      await page.goto(url);
+      screenshot = await page.screenshot({
+        path: "TypeSearch.png",
+      });
+      fs.writeFileSync("TypeSearch.png", screenshot);
+      await page.close();
+      await browser.close();
+      break;
+  }
 }
+
 async.eachSeries(
-  [
-    "",
-    "FilterTypeMovie",
-    "FilterTypeVideo",
-    "FilterTypeChannel",
-    "FilterTypePlaylist",
-  ],
+  ["", "TypeMovie", "TypeVideo", "TypeChannel", "TypePlaylist"],
   async (type) => await TypeTube("Angel Numbers", type)
 );
