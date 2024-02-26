4ba4c59f32cce38a0b3576f5eacbe8cc4741086d
yt-dlx <yt-dlx@proton.me>
2024-02-26 02:45:57 -0500

    yt-dlx @2.0.0

--- web.mjs
+++ web.mjs
@@ -1,7 +1,13 @@
 import fs from "fs";
 import async from "async";
+import colors from "colors";
+import { load } from "cheerio";
+import getId from "get-video-id";
+import spinClient from "spinnies";
 import puppeteer from "puppeteer";
+import { randomUUID } from "crypto";
 
+const spinnies = new spinClient();
 async function TypeTube(query, type = "TypeSearch") {
   const browser = await puppeteer.launch({
     userDataDir: "other",
@@ -13,7 +19,21 @@ async function TypeTube(query, type = "TypeSearch") {
   );
   let url;
   let screenshot;
+  const spin = randomUUID();
   switch (type) {
+    case "TypeMovie":
+      url =
+        "https://www.youtube.com/results?search_query=" +
+        decodeURIComponent(query) +
+        "&sp=EgIQBA%253D%253D";
+      await page.goto(url);
+      screenshot = await page.screenshot({
+        path: "TypeMovie.png",
+      });
+      fs.writeFileSync("TypeMovie.png", screenshot);
+      await page.close();
+      await browser.close();
+      break;
     case "TypeVideo":
       url =
         "https://www.youtube.com/results?search_query=" +
@@ -53,28 +73,78 @@ async function TypeTube(query, type = "TypeSearch") {
       await page.close();
       await browser.close();
       break;
-    case "TypeMovie":
-      url =
-        "https://www.youtube.com/results?search_query=" +
-        decodeURIComponent(query) +
-        "&sp=EgIQBA%253D%253D";
-      await page.goto(url);
-      screenshot = await page.screenshot({
-        path: "TypeMovie.png",
-      });
-      fs.writeFileSync("TypeMovie.png", screenshot);
-      await page.close();
-      await browser.close();
-      break;
+    case "TypeSearch":
     default:
+      let TypeSearchData = [];
+      spinnies.add(spin, {
+        text: colors.green("@scrape: ") + "booting chromium...",
+      });
       url =
         "https://www.youtube.com/results?search_query=" +
         decodeURIComponent(query);
       await page.goto(url);
+      spinnies.update(spin, {
+        text: colors.yellow("@scrape: ") + "waiting for hydration...",
+      });
       screenshot = await page.screenshot({
         path: "TypeSearch.png",
       });
       fs.writeFileSync("TypeSearch.png", screenshot);
+      spinnies.update(spin, {
+        text: colors.yellow("@scrape: ") + "took screenshot...",
+      });
+      for (let i = 0; i < 5; i++) {
+        await page.evaluate(() => window.scrollBy(0, window.innerHeight));
+      }
+      const content = await page.content();
+      const $ = load(content);
+      const videoElements = $(
+        "ytd-video-renderer:not([class*='ytd-rich-grid-video-renderer'])"
+      );
+      videoElements.each(async (_, vide) => {
+        const videoId = getId(
+          "https://www.youtube.com" + $(vide).find("a").attr("href")
+        ).id;
+        const authorContainer = $(vide).find(".ytd-channel-name a");
+        const uploadedOnElement = $(vide).find(
+          ".inline-metadata-item.style-scope.ytd-video-meta-block"
+        );
+        TypeSearchData.push({
+          title: $(vide).find("#video-title").text().trim() || undefined,
+          views:
+            $(vide)
+              .find(".inline-metadata-item.style-scope.ytd-video-meta-block")
+              .filter((_, vide) => $(vide).text().includes("views"))
+              .text()
+              .trim()
+              .replace(/ views/g, "") || undefined,
+          author: authorContainer.text().trim() || undefined,
+          videoId,
+          uploadOn:
+            uploadedOnElement.length >= 2
+              ? $(uploadedOnElement[1]).text().trim()
+              : undefined,
+          authorUrl:
+            "https://www.youtube.com" + authorContainer.attr("href") ||
+            undefined,
+          videoLink: "https://www.youtube.com/watch?v=" + videoId,
+          thumbnailUrls: [
+            `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`,
+            `https://img.youtube.com/vi/${videoId}/sddefault.jpg`,
+            `https://img.youtube.com/vi/${videoId}/mqdefault.jpg`,
+            `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`,
+            `https://img.youtube.com/vi/${videoId}/default.jpg`,
+          ],
+          description:
+            $(vide).find(".metadata-snippet-text").text().trim() || undefined,
+        });
+      });
+      console.log(TypeSearchData);
+      spinnies.succeed(spin, {
+        text:
+          colors.yellow("@info: ") +
+          colors.white("scrapping done, total videos " + TypeSearchData.length),
+      });
       await page.close();
       await browser.close();
       break;
@@ -82,6 +152,6 @@ async function TypeTube(query, type = "TypeSearch") {
 }
 
 async.eachSeries(
-  ["", "TypeMovie", "TypeVideo", "TypeChannel", "TypePlaylist"],
+  ["", "TypeSearch", "TypeMovie", "TypeVideo", "TypeChannel", "TypePlaylist"],
   async (type) => await TypeTube("Angel Numbers", type)
 );
