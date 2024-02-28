6f13d6072528d76342d9c6bfcebad5ba73f364ac
yt-dlx <yt-dlx@proton.me>
2024-02-28 21:49:20 +0530

    yt-dlx @2.0.4

--- scripts/base/grabber.ts
+++ scripts/base/grabber.ts
@@ -1,55 +1,55 @@
-import colors from "colors";
-import { chromium } from "playwright";
-import type { Browser } from "playwright";
+// import colors from "colors";
+// import { chromium } from "playwright";
+// import type { Browser } from "playwright";
 
-interface grabber {
-  domain: string;
-  query: string;
-  route: string;
-}
-export default async function grabber({
-  query,
-  route,
-  domain,
-}: grabber): Promise<string | null> {
-  const browser: Browser = await chromium.launch({ headless: true });
-  try {
-    const host = `${decodeURIComponent(domain)}/${decodeURIComponent(
-      route
-    )}?query=${decodeURIComponent(query)}`;
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto(host);
-    await page.waitForSelector("button[class*=ring-blue-600]", {
-      timeout: 10000,
-    });
-    await page.click("button[class*=ring-blue-600]");
-    const requestFinished = new Promise((resolve) => {
-      page.on("requestfinished", async (request) => {
-        try {
-          if (request.url().includes("/" + route)) {
-            const response = await request.response();
-            if (response) {
-              const json = await response.json();
-              resolve(json);
-            } else resolve(null);
-          }
-        } catch (error) {
-          console.log(colors.red("@error:"), error);
-          resolve(null);
-        }
-      });
-    });
-    const payLoad = await requestFinished;
-    if (payLoad) {
-      await browser.close();
-      return JSON.stringify(payLoad);
-    } else {
-      await browser.close();
-      return null;
-    }
-  } catch (error) {
-    console.log(colors.red("@error:"), error);
-    return null;
-  }
-}
+// interface grabber {
+// domain: string;
+// query: string;
+// route: string;
+// }
+// export default async function grabber({
+// query,
+// route,
+// domain,
+// }: grabber): Promise<string | null> {
+// const browser: Browser = await chromium.launch({ headless: true });
+// try {
+// const host = `${decodeURIComponent(domain)}/${decodeURIComponent(
+// route
+// )}?query=${decodeURIComponent(query)}`;
+// const context = await browser.newContext();
+// const page = await context.newPage();
+// await page.goto(host);
+// await page.waitForSelector("button[class*=ring-blue-600]", {
+// timeout: 10000,
+// });
+// await page.click("button[class*=ring-blue-600]");
+// const requestFinished = new Promise((resolve) => {
+// page.on("requestfinished", async (request) => {
+// try {
+// if (request.url().includes("/" + route)) {
+// const response = await request.response();
+// if (response) {
+// const json = await response.json();
+// resolve(json);
+// } else resolve(null);
+// }
+// } catch (error) {
+// console.log(colors.red("@error:"), error);
+// resolve(null);
+// }
+// });
+// });
+// const payLoad = await requestFinished;
+// if (payLoad) {
+// await browser.close();
+// return JSON.stringify(payLoad);
+// } else {
+// await browser.close();
+// return null;
+// }
+// } catch (error) {
+// console.log(colors.red("@error:"), error);
+// return null;
+// }
+// }
