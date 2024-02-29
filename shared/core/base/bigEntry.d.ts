a75694c5bb79a6d925eb4acb1c4cc271460f8c5b
yt-dlx <yt-dlx@proton.me>
2024-02-29 15:36:51 +0530

    yt-dlx @3.0.0

---
+++ shared/core/base/bigEntry.d.ts
@@ -0,0 +1,3 @@
+import type TubeConfig from "../interface/TubeConfig";
+export default function bigEntry(metaBody: TubeConfig[]): Promise<TubeConfig | undefined>;
+//# sourceMappingURL=bigEntry.d.ts.map
