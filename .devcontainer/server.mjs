4c50cc9345023010854ad237f5a2ec9003aca8d1
yt-dlx <yt-dlx@proton.me>
2024-02-29 15:39:08 +0530

    yt-dlx @3.0.0

--- .devcontainer/server.mjs
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import colors from "colors";
-
-const app = express();
-
-const port = process.env.PORT || 8080;
-app.listen(port, async () => {
-  console.log(colors.bold.green("@express:"), port);
-});
