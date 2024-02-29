cb76f9024322076d6b26e39f5325e7675d7eb47f
yt-dlx <yt-dlx@proton.me>
2024-02-29 11:30:01 +0530

    yt-dlx @2.0.4

--- .devcontainer/startup.mjs
+++ .devcontainer/startup.mjs
@@ -9,33 +9,27 @@ const colors = {
 };
 
 const scripts = {
-  setup: "chmod +x ./linux-setup.sh && ./linux-setup.sh",
-  remake: "bun run clean && bun run make && bun run build",
-  "remake:update":
-    "bun run clean && bun run make && bun install --latest && bun run build",
-  upload:
-    "bun run test && bun run cli && bun run remake && npm pkg fix && bun run publish --access=public && bun run update",
-  clean: "bun run clean:base && bun run clean:frontend",
+  "setup": "chmod +x ./linux-setup.sh && ./linux-setup.sh",
+  "remake": "bun run clean && bun run make && bun run build",
+  "remake:update": "bun run clean && bun run make && bun install --latest && bun run build",
+  "upload": "bun run test && bun run cli && bun run remake && npm pkg fix && bun run publish --access=public && bun run update",
+  "clean": "bun run clean:base && bun run clean:frontend",
   "clean:base": "rm -rf node_modules .temp shared bun.lockb",
   "clean:frontend": "cd frontend && rm -rf node_modules .next bun.lockb",
-  make: "bun run make:base && bun run make:frontend",
+  "make": "bun run make:base && bun run make:frontend",
   "make:base": "bun install",
   "make:frontend": "cd frontend && bun install",
-  build: "bun run build:base && bun run build:frontend",
-  "build:base":
-    "tsup --config 'tsup.config.ts' && rollup -c 'rollup.config.mjs'",
+  "build": "bun run build:base && bun run build:frontend",
+  "build:base": "tsup --config 'tsup.config.ts' && rollup -c 'rollup.config.mjs'",
   "build:frontend": "cd frontend && bun run build",
-  update: "bun run make && bun run update:base && bun run update:frontend",
+  "update": "bun run make && bun run update:base && bun run update:frontend",
   "update:base": "bun install --latest && bun update --latest",
-  "update:frontend":
-    "cd frontend && bun install --latest && bun update --latest",
-  cli: "bun run link && bun run test:cli && bun run unlink",
-  test: "bun run test:bun && bun run test:cli",
+  "update:frontend": "cd frontend && bun install --latest && bun update --latest",
+  "cli": "bun run link && bun run test:cli && bun run unlink",
+  "test": "bun run est:vitest && bun run test:cli",
   "test:vitest": "vitest --config 'vitest.config.mts'",
-  "test:spec":
-    "bun test --timeout 60000 --bail --watch ./scripts/__tests__/bun.spec.ts",
-  "test:cli":
-    "yt version && yt-dlx audio-lowest --query 'PERSONAL BY PLAZA' && yt-dlx al --query 'SuaeRys5tTc'",
+  "test:spec": "bun test --timeout 60000 --bail --watch ./scripts/__tests__/bun.spec.ts",
+  "test:cli": "yt version && yt-dlx audio-lowest --query 'PERSONAL BY PLAZA' && yt-dlx al --query 'SuaeRys5tTc'"
 };
 
 function runScript() {
