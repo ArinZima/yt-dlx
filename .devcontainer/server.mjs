f11ec6a96360a3138ede489d9437717717da8f43
yt-dlx <yt-dlx@proton.me>
2024-02-29 15:50:39 +0530

    yt-dlx @3.0.0

--- .devcontainer/server.mjs
+++ .devcontainer/server.mjs
@@ -1,7 +1,30 @@
+import { execSync } from "child_process";
 import express from "express";
 import colors from "colors";
 
 const app = express();
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+(async () => {
+  let counter = 1;
+  while (true) {
+    try {
+      console.clear();
+      console.log(colors.green("@info:"), "re-installing yt-dlx");
+      console.log(colors.green("@info:"), `iteration ${counter}`);
+      execSync("bun remove yt-dlx", { stdio: "inherit" });
+      execSync("bun add yt-dlx@latest", { stdio: "inherit" });
+      execSync("rm -rf node_modules bun.lockb", { stdio: "inherit" });
+      counter++;
+      await sleep(2000);
+    } catch (error) {
+      console.error(colors.red("@error:"), error.message);
+      process.exit(1);
+    }
+  }
+})();
 
 const port = process.env.PORT || 8080;
 app.listen(port, async () => {
