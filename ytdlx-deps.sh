79dc4ea5490df6c55ee5f2e34e20a2c06e215047
yt-dlx <yt-dlx@proton.me>
2024-03-01 12:30:51 +0530

    yt-dlx @3.0.3

--- ytdlx-deps.sh
+++ ytdlx-deps.sh
@@ -16,19 +16,29 @@ log_info() {
 run_command() {
     local command="$1"
     local error_message="$2"
+    if [ "$use_sudo" = true ]; then
+        command="sudo $command"
+    fi
     if ! eval "$command"; then
         log_error "$error_message"
     fi
 }
+
+if [ -x "$(command -v sudo)" ]; then
+    use_sudo=true
+else
+    use_sudo=false
+fi
+
 if [ -x "$(command -v apt)" ]; then
     log_info "Detected Debian-based system"
-    run_command "sudo apt update" "Failed to update APT repositories"
-    run_command "sudo apt install -y ffmpeg opus-tools" "Failed to install fmpeg opus-tools in debian"
-    run_command "sudo rm -rf /var/lib/apt/lists/*" "Failed to clean cache"
+    run_command "apt update" "Failed to update APT repositories"
+    run_command "apt install -y ffmpeg opus-tools" "Failed to install ffmpeg opus-tools in debian"
+    run_command "rm -rf /var/lib/apt/lists/*" "Failed to clean cache"
 elif [ -x "$(command -v pacman)" ]; then
     log_info "Detected Arch-based system"
-    run_command "yes | sudo pacman -Syu" "Failed to update system with pacman"
-    run_command "yes | sudo pacman -S ffmpeg opus-tools" "Failed to install ffmpeg opus-tools in arch"
-    run_command "sudo rm -rf /var/cache/pkgfile/*" "Failed to clean cache"
+    run_command "yes | pacman -Syu" "Failed to update system with pacman"
+    run_command "yes | pacman -S ffmpeg opus-tools" "Failed to install ffmpeg opus-tools in arch"
+    run_command "rm -rf /var/cache/pkgfile/*" "Failed to clean cache"
 fi
 echo -e "${GREEN}ytdlx-deps.sh execution completed${NC}"
