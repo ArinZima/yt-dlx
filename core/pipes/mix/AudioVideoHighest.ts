0d61c9c4d50d90a2386616ef0a0037f521900a43
yt-dlx <yt-dlx@proton.me>
2024-03-01 16:52:27 +0530

    yt-dlx @3.0.3

--- core/pipes/mix/AudioVideoHighest.ts
+++ core/pipes/mix/AudioVideoHighest.ts
@@ -59,12 +59,12 @@ export default async function AudioVideoHighest(
     }
     proc.addInput(VmetaEntry.AVDownload.mediaurl);
     proc.addInput(AmetaEntry.AVDownload.mediaurl);
-    proc.addOption("-spatial-aq", "1");
-    proc.addOption("-preset", "slow");
-    proc.addOption("-level:v", "4.2");
-    proc.addOption("-rc", "vbr_hq");
-    proc.addOption("-b:v", "10M");
-    proc.addOption("-shortest");
+    proc.addOutputOption("-spatial-aq", "1");
+    proc.addOutputOption("-preset", "slow");
+    proc.addOutputOption("-level:v", "4.2");
+    proc.addOutputOption("-rc", "vbr_hq");
+    proc.addOutputOption("-b:v", "10M");
+    proc.addOutputOption("-shortest");
     proc.format(outputFormat);
     proc.on("start", (command) => {
       if (verbose) console.log(command);
