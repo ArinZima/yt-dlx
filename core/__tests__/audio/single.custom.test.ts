1ec5cf83cfc8001fdd90804c696131db4a98b4d2
yt-dlx <yt-dlx@proton.me>
2024-03-01 17:36:26 +0530

    yt-dlx @3.0.3

--- core/__tests__/audio/single.custom.test.ts
+++ core/__tests__/audio/single.custom.test.ts
@@ -2,90 +2,133 @@ import * as fs from "fs";
 import ytdlx from "../..";
 import fsx from "fs-extra";
 import colors from "colors";
-import * as bun from "bun:test";
-// =======================================================[PASS-TEST]=======================================================
-bun.test(colors.blue("\n\n@tesing: ") + "AutoDownloadTest()", async () => {
-  try {
-    let holder: any;
-    console.log(colors.bold.yellow("@test:"), "ytdlx.audio.single.custom()");
-    console.log(colors.bold.yellow("@info:"), "stream: false");
-    holder = await ytdlx.audio.single.custom({
-      query: "sQEgklEwhSo",
-      outputFormat: "ogg",
-      quality: "medium",
-      folderName: "audio",
-      stream: false,
-    });
-    if (holder) {
-      console.log(colors.bold.green("@pass:"), holder);
-      await fsx.remove("audio");
-    } else {
-      await fsx.remove("audio");
-      throw (colors.bold.red("@error:"), holder);
+import async from "async";
+
+async.series(
+  [
+    async function (callback) {
+      try {
+        let holder: any;
+        console.log(
+          colors.bold.yellow("@test:"),
+          "ytdlx.audio.single.custom()"
+        );
+        console.log(colors.bold.yellow("@info:"), "stream: false");
+        holder = await ytdlx.audio.single.custom({
+          query: "sQEgklEwhSo",
+          outputFormat: "ogg",
+          quality: "medium",
+          folderName: "audio",
+          stream: false,
+        });
+        if (holder) {
+          console.log(colors.bold.green("@pass:"), holder);
+          await fsx.remove("audio");
+          callback(null, holder);
+        } else {
+          await fsx.remove("audio");
+          callback(new Error(colors.bold.red("@error:"), holder));
+        }
+      } catch (error: any) {
+        await fsx.remove("audio");
+        callback(error);
+      }
+    },
+    // =======================================================[BREAK-TEST]=======================================================
+    async function (callback) {
+      try {
+        let holder: any;
+        console.log(
+          colors.bold.yellow("@test:"),
+          "ytdlx.audio.single.custom()"
+        );
+        console.log(colors.bold.yellow("@info:"), "stream: false");
+        holder = await ytdlx.audio.single.custom({
+          query: "https://youtu.be/sQEgklEwhSo?si=vuiHFaNCpYvMigWq",
+          outputFormat: "mp3",
+          filter: "nightcore",
+          quality: "medium",
+          folderName: "audio",
+          stream: false,
+        });
+        if (holder) {
+          console.log(colors.bold.green("@pass:"), holder);
+          await fsx.remove("audio");
+          callback(null, holder);
+        } else {
+          await fsx.remove("audio");
+          callback(new Error(colors.bold.red("@error:"), holder));
+        }
+      } catch (error: any) {
+        await fsx.remove("audio");
+        callback(error);
+      }
+    },
+    // =======================================================[BREAK-TEST]=======================================================
+    async function (callback) {
+      try {
+        let holder: any;
+        console.log(
+          colors.bold.yellow("@test:"),
+          "ytdlx.audio.single.custom()"
+        );
+        console.log(colors.bold.yellow("@info:"), "stream: true");
+        holder = await ytdlx.audio.single.custom({
+          query: "sQEgklEwhSo",
+          outputFormat: "flac",
+          quality: "medium",
+          folderName: "audio",
+          stream: true,
+        });
+        if (holder.stream && holder.filename) {
+          holder.stream.pipe(fs.createWriteStream(holder.filename));
+          console.log(colors.bold.green("@pass:"), holder.filename);
+          await fsx.remove("audio");
+          callback(null, holder);
+        } else {
+          await fsx.remove("audio");
+          callback(new Error(colors.bold.red("@error:"), holder));
+        }
+      } catch (error: any) {
+        await fsx.remove("audio");
+        callback(error);
+      }
+    },
+    // =======================================================[BREAK-TEST]=======================================================
+    async function (callback) {
+      try {
+        let holder: any;
+        console.log(
+          colors.bold.yellow("@test:"),
+          "ytdlx.audio.single.custom()"
+        );
+        console.log(colors.bold.yellow("@info:"), "stream: true");
+        holder = await ytdlx.audio.single.custom({
+          query: "https://youtu.be/sQEgklEwhSo?si=vuiHFaNCpYvMigWq",
+          outputFormat: "aiff",
+          filter: "bassboost",
+          quality: "medium",
+          folderName: "audio",
+          stream: true,
+        });
+        if (holder.stream && holder.filename) {
+          holder.stream.pipe(fs.createWriteStream(holder.filename));
+          console.log(colors.bold.green("@pass:"), holder.filename);
+          await fsx.remove("audio");
+          callback(null, holder);
+        } else {
+          await fsx.remove("audio");
+          callback(new Error(colors.bold.red("@error:"), holder));
+        }
+      } catch (error: any) {
+        await fsx.remove("audio");
+        callback(error);
+      }
+    },
+  ],
+  function (err) {
+    if (err) {
+      throw new Error(colors.bold.red("@error:"), err);
     }
-    console.log(colors.bold.yellow("@test:"), "ytdlx.audio.single.custom()");
-    console.log(colors.bold.yellow("@info:"), "stream: false");
-    holder = await ytdlx.audio.single.custom({
-      query: "https://youtu.be/sQEgklEwhSo?si=vuiHFaNCpYvMigWq",
-      outputFormat: "mp3",
-      filter: "nightcore",
-      quality: "medium",
-      folderName: "audio",
-      stream: false,
-    });
-    if (holder) {
-      console.log(colors.bold.green("@pass:"), holder);
-      await fsx.remove("audio");
-    } else {
-      await fsx.remove("audio");
-      throw (colors.bold.red("@error:"), holder);
-    }
-  } catch (error) {
-    await fsx.remove("audio");
-    throw (colors.bold.red("@error:"), error);
   }
-});
-// =======================================================[PASS-TEST]=======================================================
-bun.test(colors.blue("\n\n@tesing: ") + "StreamingTest()", async () => {
-  try {
-    let holder: any;
-    console.log(colors.bold.yellow("@test:"), "ytdlx.audio.single.custom()");
-    console.log(colors.bold.yellow("@info:"), "stream: true");
-    holder = await ytdlx.audio.single.custom({
-      query: "sQEgklEwhSo",
-      outputFormat: "flac",
-      quality: "medium",
-      folderName: "audio",
-      stream: true,
-    });
-    if (holder.stream && holder.filename) {
-      holder.stream.pipe(fs.createWriteStream(holder.filename));
-      console.log(colors.bold.green("@pass:"), holder.filename);
-      await fsx.remove("audio");
-    } else {
-      await fsx.remove("audio");
-      throw (colors.bold.red("@error:"), holder);
-    }
-    console.log(colors.bold.yellow("@test:"), "ytdlx.audio.single.custom()");
-    console.log(colors.bold.yellow("@info:"), "stream: true");
-    holder = await ytdlx.audio.single.custom({
-      query: "https://youtu.be/sQEgklEwhSo?si=vuiHFaNCpYvMigWq",
-      outputFormat: "aiff",
-      filter: "bassboost",
-      quality: "medium",
-      folderName: "audio",
-      stream: true,
-    });
-    if (holder.stream && holder.filename) {
-      holder.stream.pipe(fs.createWriteStream(holder.filename));
-      console.log(colors.bold.green("@pass:"), holder.filename);
-      await fsx.remove("audio");
-    } else {
-      await fsx.remove("audio");
-      throw (colors.bold.red("@error:"), holder);
-    }
-  } catch (error) {
-    await fsx.remove("audio");
-    throw (colors.bold.red("@error:"), error);
-  }
-});
+);
--- core/__tests__/audio/single.lowest.test.ts
+++ core/__tests__/audio/single.lowest.test.ts
@@ -2,86 +2,129 @@ import * as fs from "fs";
 import ytdlx from "../..";
 import fsx from "fs-extra";
 import colors from "colors";
-import * as bun from "bun:test";
-// =======================================================[PASS-TEST]=======================================================
-bun.test(colors.blue("\n\n@tesing: ") + "AutoDownloadTest()", async () => {
-  try {
-    let holder: any;
-    console.log(colors.bold.yellow("@test:"), "ytdlx.audio.single.lowest()");
-    console.log(colors.bold.yellow("@info:"), "stream: false");
-    holder = await ytdlx.audio.single.lowest({
-      query: "sQEgklEwhSo",
-      outputFormat: "ogg",
-      folderName: "audio",
-      stream: false,
-    });
-    if (holder) {
-      console.log(colors.bold.green("@pass:"), holder);
-      await fsx.remove("audio");
-    } else {
-      await fsx.remove("audio");
-      throw (colors.bold.red("@error:"), holder);
+import async from "async";
+
+async.series(
+  [
+    async function (callback) {
+      try {
+        let holder: any;
+        console.log(
+          colors.bold.yellow("@test:"),
+          "ytdlx.audio.single.lowest()"
+        );
+        console.log(colors.bold.yellow("@info:"), "stream: false");
+        holder = await ytdlx.audio.single.lowest({
+          query: "sQEgklEwhSo",
+          outputFormat: "ogg",
+          folderName: "audio",
+          stream: false,
+        });
+        if (holder) {
+          console.log(colors.bold.green("@pass:"), holder);
+          await fsx.remove("audio");
+          callback(null, holder);
+        } else {
+          await fsx.remove("audio");
+          callback(new Error(colors.bold.red("@error:"), holder));
+        }
+      } catch (error: any) {
+        await fsx.remove("audio");
+        callback(error);
+      }
+    },
+    // =======================================================[BREAK-TEST]=======================================================
+    async function (callback) {
+      try {
+        let holder: any;
+        console.log(
+          colors.bold.yellow("@test:"),
+          "ytdlx.audio.single.lowest()"
+        );
+        console.log(colors.bold.yellow("@info:"), "stream: false");
+        holder = await ytdlx.audio.single.lowest({
+          query: "https://youtu.be/sQEgklEwhSo?si=vuiHFaNCpYvMigWq",
+          outputFormat: "mp3",
+          filter: "nightcore",
+          folderName: "audio",
+          stream: false,
+        });
+        if (holder) {
+          console.log(colors.bold.green("@pass:"), holder);
+          await fsx.remove("audio");
+          callback(null, holder);
+        } else {
+          await fsx.remove("audio");
+          callback(new Error(colors.bold.red("@error:"), holder));
+        }
+      } catch (error: any) {
+        await fsx.remove("audio");
+        callback(error);
+      }
+    },
+    // =======================================================[BREAK-TEST]=======================================================
+    async function (callback) {
+      try {
+        let holder: any;
+        console.log(
+          colors.bold.yellow("@test:"),
+          "ytdlx.audio.single.lowest()"
+        );
+        console.log(colors.bold.yellow("@info:"), "stream: true");
+        holder = await ytdlx.audio.single.lowest({
+          query: "sQEgklEwhSo",
+          outputFormat: "flac",
+          folderName: "audio",
+          stream: true,
+        });
+        if (holder.stream && holder.filename) {
+          holder.stream.pipe(fs.createWriteStream(holder.filename));
+          console.log(colors.bold.green("@pass:"), holder.filename);
+          await fsx.remove("audio");
+          callback(null, holder);
+        } else {
+          await fsx.remove("audio");
+          callback(new Error(colors.bold.red("@error:"), holder));
+        }
+      } catch (error: any) {
+        await fsx.remove("audio");
+        callback(error);
+      }
+    },
+    // =======================================================[BREAK-TEST]=======================================================
+    async function (callback) {
+      try {
+        let holder: any;
+        console.log(
+          colors.bold.yellow("@test:"),
+          "ytdlx.audio.single.lowest()"
+        );
+        console.log(colors.bold.yellow("@info:"), "stream: true");
+        holder = await ytdlx.audio.single.lowest({
+          query: "https://youtu.be/sQEgklEwhSo?si=vuiHFaNCpYvMigWq",
+          outputFormat: "aiff",
+          filter: "bassboost",
+          folderName: "audio",
+          stream: true,
+        });
+        if (holder.stream && holder.filename) {
+          holder.stream.pipe(fs.createWriteStream(holder.filename));
+          console.log(colors.bold.green("@pass:"), holder.filename);
+          await fsx.remove("audio");
+          callback(null, holder);
+        } else {
+          await fsx.remove("audio");
+          callback(new Error(colors.bold.red("@error:"), holder));
+        }
+      } catch (error: any) {
+        await fsx.remove("audio");
+        callback(error);
+      }
+    },
+  ],
+  function (err) {
+    if (err) {
+      throw new Error(colors.bold.red("@error:"), err);
     }
-    console.log(colors.bold.yellow("@test:"), "ytdlx.audio.single.lowest()");
-    console.log(colors.bold.yellow("@info:"), "stream: false");
-    holder = await ytdlx.audio.single.lowest({
-      query: "https://youtu.be/sQEgklEwhSo?si=vuiHFaNCpYvMigWq",
-      outputFormat: "mp3",
-      filter: "nightcore",
-      folderName: "audio",
-      stream: false,
-    });
-    if (holder) {
-      console.log(colors.bold.green("@pass:"), holder);
-      await fsx.remove("audio");
-    } else {
-      await fsx.remove("audio");
-      throw (colors.bold.red("@error:"), holder);
-    }
-  } catch (error) {
-    await fsx.remove("audio");
-    throw (colors.bold.red("@error:"), error);
   }
-});
-// =======================================================[PASS-TEST]=======================================================
-bun.test(colors.blue("\n\n@tesing: ") + "StreamingTest()", async () => {
-  try {
-    let holder: any;
-    console.log(colors.bold.yellow("@test:"), "ytdlx.audio.single.lowest()");
-    console.log(colors.bold.yellow("@info:"), "stream: true");
-    holder = await ytdlx.audio.single.lowest({
-      query: "sQEgklEwhSo",
-      outputFormat: "flac",
-      folderName: "audio",
-      stream: true,
-    });
-    if (holder.stream && holder.filename) {
-      holder.stream.pipe(fs.createWriteStream(holder.filename));
-      console.log(colors.bold.green("@pass:"), holder.filename);
-      await fsx.remove("audio");
-    } else {
-      await fsx.remove("audio");
-      throw (colors.bold.red("@error:"), holder);
-    }
-    console.log(colors.bold.yellow("@test:"), "ytdlx.audio.single.lowest()");
-    console.log(colors.bold.yellow("@info:"), "stream: true");
-    holder = await ytdlx.audio.single.lowest({
-      query: "https://youtu.be/sQEgklEwhSo?si=vuiHFaNCpYvMigWq",
-      outputFormat: "aiff",
-      filter: "bassboost",
-      folderName: "audio",
-      stream: true,
-    });
-    if (holder.stream && holder.filename) {
-      holder.stream.pipe(fs.createWriteStream(holder.filename));
-      console.log(colors.bold.green("@pass:"), holder.filename);
-      await fsx.remove("audio");
-    } else {
-      await fsx.remove("audio");
-      throw (colors.bold.red("@error:"), holder);
-    }
-  } catch (error) {
-    await fsx.remove("audio");
-    throw (colors.bold.red("@error:"), error);
-  }
-});
+);
