777b36117d9f543e4a6462534a64858c81516d1d
yt-dlx <yt-dlx@proton.me>
2024-03-01 11:25:17 +0530

    yt-dlx @3.0.3

--- .devcontainer/startup.mjs
+++ .devcontainer/startup.mjs
@@ -9,32 +9,27 @@ const colors = {
 };
 
 const core = {
-  rerun: "bun ./.devcontainer/server.mjs & bun ./.devcontainer/rerun.mjs",
-  remake:
-    "bun run clean && bun run make && bun install --latest && bun run build",
-  upload:
-    "bun run test && bun run cli && bun run build && npm pkg fix && npm publish --access=public && bun run update",
-  clean: "bun run clean:base && bun run clean:client",
+  "rerun": "bun ./.devcontainer/server.mjs & bun ./.devcontainer/rerun.mjs",
+  "remake": "bun run clean && bun run make && bun install --latest && bun run build",
+  "upload": "bun run test && bun run cli && bun run build && npm pkg fix && npm publish --access=public && bun run update",
+  "clean": "bun run clean:base && bun run clean:client",
   "clean:base": "rm -rf node_modules .temp shared bun.lockb",
   "clean:client": "cd client && rm -rf node_modules .next bun.lockb",
-  make: "bun run make:deps && bun run make:base && bun run make:client",
+  "make": "bun run make:deps && bun run make:base && bun run make:client",
   "make:base": "bun install",
   "make:client": "cd client && bun install",
   "make:deps": "chmod +x ./ytdlx-deps.sh && ./ytdlx-deps.sh",
-  build: "bun run build:base && bun run build:client",
-  "build:base":
-    "tsup --config 'tsup.config.ts' && rollup -c 'rollup.config.mjs'",
+  "build": "bun run build:base && bun run build:client",
+  "build:base": "tsup --config 'tsup.config.ts' && rollup -c 'rollup.config.mjs'",
   "build:client": "cd client && npm run build",
-  update: "bun run make && bun run update:base && bun run update:client",
+  "update": "bun run make && bun run update:base && bun run update:client",
   "update:base": "bun install --latest && bun update --latest",
   "update:client": "cd client && bun install --latest && bun update --latest",
-  cli: "bun link && bun run test:cli && bun unlink",
-  test: "bun run test:bun && bun run test:cli",
+  "cli": "bun link && bun run test:cli && bun unlink",
+  "test": "bun run test:bun && bun run test:cli",
   "test:bun": "bun test --timeout 120000 --bail --watch",
-  "test:spec":
-    "tsup --config 'tsup.config.ts' './core/__tests__/bun.spec.ts' --outDir '.temp' --clean && node .temp/bun.spec.js",
-  "test:cli":
-    "yt version && yt-dlx audio-lowest --query 'PERSONAL BY PLAZA' && yt-dlx al --query 'SuaeRys5tTc'",
+  "test:cli": "yt version && yt-dlx audio-lowest --query 'PERSONAL BY PLAZA' && yt-dlx al --query 'SuaeRys5tTc'",
+  "test:spec": "tsup --config 'tsup.config.ts' './core/__tests__/bun.spec.ts' --outDir '.temp' --clean && node .temp/bun.spec.js"
 };
 
 function runScript() {
