96b19cc55f743e0d35d26a8df0623feafa5940a0
yt-dlx <yt-dlx@proton.me>
2024-03-01 18:18:39 +0530

    yt-dlx @3.0.3

--- core/__tests__/runner.ts
+++ core/__tests__/runner.ts
@@ -10,7 +10,7 @@ function runTestFiles(folderPath: string) {
     const filePath = join(folderPath, file);
     if (lstatSync(filePath).isDirectory()) runTestFiles(filePath);
     else if (file.endsWith(".test.js")) {
-      console.log(colors.bold.yellow("@testing:"), filePath);
+      console.log(colors.bold.yellow("\n\n@testing:"), filePath);
       const result = spawnSync("node", [filePath], { stdio: "inherit" });
       if (result.error) console.error(colors.bold.red("@error:"), result.error);
     }
