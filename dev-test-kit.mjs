29d3325cde0a5a8dfd972dc37622bd22b8a97512
yt-dlx <yt-dlx@proton.me>
2024-03-02 13:28:08 +0530

    yt-dlx @3.0.6

--- dev-test-kit.mjs
+++ dev-test-kit.mjs
@@ -119,7 +119,7 @@ function runScript() {
               `${colors.red}@error:${colors.reset}`,
               `Exited with code ${code}`
             );
-          } else console.clear();
+          }
           runScript();
         });
       } else {
