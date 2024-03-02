04aa58a52b950175f053f090d9d97f7d96ac749f
yt-dlx <yt-dlx@proton.me>
2024-03-03 02:07:50 +0530

    yt-dlx @4.3.0

--- shared/core/base/Agent.d.ts
+++
@@ -1,6 +0,0 @@
-import type EngineResult from "../interface/EngineResult";
-export default function Agent({ query, verbose, }: {
-    query: string;
-    verbose?: boolean;
-}): Promise<EngineResult>;
-//# sourceMappingURL=Agent.d.ts.map
