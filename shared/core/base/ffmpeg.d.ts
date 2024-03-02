4f0dbb25b2def0cd8e6798b0c8222f35ebb0df12
yt-dlx <yt-dlx@proton.me>
2024-03-02 22:08:15 +0530

    yt-dlx @4.0.0

---
+++ shared/core/base/ffmpeg.d.ts
@@ -0,0 +1,5 @@
+import type { FfmpegCommand } from "fluent-ffmpeg";
+declare function gpuffmpeg(input: string): FfmpegCommand;
+export default gpuffmpeg;
+export type { FfmpegCommand as gpuffmpegCommand };
+//# sourceMappingURL=ffmpeg.d.ts.map
