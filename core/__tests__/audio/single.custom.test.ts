b07563edd60032b1d5be50c8c7ce120e57f3cf04
yt-dlx <yt-dlx@proton.me>
2024-03-02 22:09:04 +0530

    yt-dlx @4.0.0

---
+++ core/__tests__/audio/single.custom.test.ts
@@ -0,0 +1,47 @@
+import * as fs from "fs";
+import ytdlx from "../..";
+import colors from "colors";
+import * as async from "async";
+
+let holder: any;
+async.series([
+  async () => {
+    try {
+      holder = await ytdlx.audio.custom({
+        folderName: ".temp/audio",
+        query: "sQEgklEwhSo",
+        quality: "medium",
+        verbose: true,
+        stream: false,
+      });
+      console.log(colors.bold.green("@pass:"), true);
+    } catch (error: any) {
+      throw new Error(colors.bold.red("@error:"), error);
+    }
+  },
+  // =========================[FULL-TEST]=========================
+  async () => {
+    try {
+      holder = await ytdlx.audio.custom({
+        folderName: ".temp/audio",
+        query: "sQEgklEwhSo",
+        quality: "medium",
+        verbose: true,
+        stream: true,
+      });
+      if (holder) {
+        await holder.ffmpeg
+          .pipe(fs.createWriteStream(holder.filename))
+          .on("finish", () => {
+            console.log(
+              colors.bold.green("@pass:"),
+              "filename",
+              holder.filename
+            );
+          });
+      } else throw new Error(colors.bold.red("@error:"), holder);
+    } catch (error: any) {
+      throw new Error(colors.bold.red("@error:"), error);
+    }
+  },
+]);
---
+++ core/base/ffmpeg.ts
@@ -0,0 +1,59 @@
+import colors from "colors";
+import * as path from "path";
+import fluent from "fluent-ffmpeg";
+import { execSync } from "child_process";
+import progressBar from "../base/progressBar";
+import type { FfmpegCommand } from "fluent-ffmpeg";
+
+function gpuffmpeg(input: string): FfmpegCommand {
+  const getTerm = (command: string) => {
+    try {
+      return execSync(command).toString().trim();
+    } catch (error) {
+      return undefined;
+    }
+  };
+  // =====================================[FFMPEG-LOGIC]=====================================
+  const ffmpeg: FfmpegCommand = fluent()
+    .setFfprobePath(path.join(__dirname, "util", "ffmpeg", "bin", "ffprobe"))
+    .setFfmpegPath(path.join(__dirname, "util", "ffmpeg", "bin", "ffmpeg"))
+    .input(input)
+    .on("start", () => {
+      progressBar({ timemark: undefined, percent: undefined });
+    })
+    .on("progress", ({ percent, timemark }) => {
+      progressBar({ timemark, percent });
+    })
+    .on("end", () => {
+      console.log(colors.green("@ffmpeg:"), "completed");
+      progressBar({ timemark: undefined, percent: undefined });
+    })
+    .on("error", (error) => {
+      console.error(colors.red("@ffmpeg:"), error.message);
+      progressBar({ timemark: undefined, percent: undefined });
+    });
+
+  // =====================================[GPU-LOGIC]=====================================
+  let gpuVendor: string | undefined;
+  try {
+    gpuVendor = getTerm("nvidia-smi --query-gpu=name --format=csv,noheader");
+  } catch (error) {
+    gpuVendor = undefined;
+  }
+  switch (true) {
+    case gpuVendor && gpuVendor.includes("NVIDIA"):
+      console.log(colors.green("@ffmpeg: using GPU " + gpuVendor));
+      ffmpeg.withInputOption("-hwaccel cuda");
+      ffmpeg.withVideoCodec("h264_nvenc");
+      break;
+    default:
+      console.log(
+        colors.yellow("@ffmpeg:"),
+        "GPU vendor not recognized.",
+        "defaulting to software processing."
+      );
+  }
+  return ffmpeg;
+}
+export default gpuffmpeg;
+export type { FfmpegCommand as gpuffmpegCommand };
