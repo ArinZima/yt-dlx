30d1c4182069e79acbcdcb86782c018526fa3dae
yt-dlx <yt-dlx@proton.me>
2024-03-02 22:08:53 +0530

    yt-dlx @4.0.0

--- core/interface/SuccessResult.ts
+++
@@ -1,3 +0,0 @@
-export default interface SuccessResult {
-  status: 200;
-}
--- shared/core/pipes/command/extract_playlist_videos.d.ts
+++
@@ -1,5 +0,0 @@
-import type EngineResult from "../../interface/EngineResult";
-export default function extract_playlist_videos({ playlistUrls, }: {
-    playlistUrls: string[];
-}): Promise<EngineResult[]>;
-//# sourceMappingURL=extract_playlist_videos.d.ts.map
