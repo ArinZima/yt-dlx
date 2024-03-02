b8acfe29583e88f2f1810304b2454086589d47ee
yt-dlx <yt-dlx@proton.me>
2024-03-03 02:00:50 +0530

    yt-dlx @4.2.0

--- shared/core/interface/StreamResult.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { Readable } from "stream";
-export default interface StreamResult {
-    stream: Readable;
-    filename: string;
-}
-//# sourceMappingURL=StreamResult.d.ts.map
