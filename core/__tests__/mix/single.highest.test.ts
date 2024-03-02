7031bed016d82e26c6a501364ef8c8076a80aec5
yt-dlx <yt-dlx@proton.me>
2024-03-02 18:29:20 +0530

    yt-dlx @3.0.6

--- core/__tests__/mix/single.highest.test.ts
+++ core/__tests__/mix/single.highest.test.ts
@@ -22,46 +22,8 @@ async.series([
   async () => {
     try {
       holder = await ytdlx.audio_video.highest({
-        query: "sQEgklEwhSo",
-        folderName: ".temp/audio_video",
-        verbose: true,
-        stream: false,
-      });
-      console.log(colors.bold.green("@pass:"), true);
-    } catch (error: any) {
-      throw new Error(colors.bold.red("@error:"), error);
-    }
-  },
-  // =========================[FULL-TEST]=========================
-  async () => {
-    try {
-      holder = await ytdlx.audio_video.highest({
         folderName: ".temp/audio_video",
         query: "sQEgklEwhSo",
-        verbose: true,
-        stream: true,
-      });
-      if (holder) {
-        await holder.ffmpeg
-          .pipe(fs.createWriteStream(holder.filename))
-          .on("finish", () => {
-            console.log(
-              colors.bold.green("@pass:"),
-              "filename",
-              holder.filename
-            );
-          });
-      } else throw new Error(colors.bold.red("@error:"), holder);
-    } catch (error: any) {
-      throw new Error(colors.bold.red("@error:"), error);
-    }
-  },
-  // =========================[FULL-TEST]=========================
-  async () => {
-    try {
-      holder = await ytdlx.audio_video.highest({
-        query: "sQEgklEwhSo",
-        folderName: ".temp/audio_video",
         verbose: true,
         stream: true,
       });
