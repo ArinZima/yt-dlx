e751d75f548c836713e8fd13a38ed5d33ee1d1f3
yt-dlx <yt-dlx@proton.me>
2024-03-02 20:39:15 +0530

    yt-dlx @3.0.9

--- util/engine.mjs
+++ util/engine.mjs
@@ -1,77 +1,27 @@
 import fetch from "node-fetch";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-import { spawn } from "child_process";
-import { createWriteStream, existsSync, renameSync } from "fs";
+import { createWriteStream } from "fs";
 
-const downloadAndExtract = async () => {
+const downloadAndRename = async () => {
   try {
     const __filename = fileURLToPath(import.meta.url);
-    const filepath = join(dirname(__filename), "ffmpeg.tar.xz");
-    let extractProcess;
-    switch (true) {
-      case existsSync(filepath):
-        extractProcess = spawn("tar", [
-          "xf",
-          filepath,
-          "-C",
-          dirname(__filename),
-        ]);
-        extractProcess.on("exit", (code) => {
-          if (code === 0) {
-            const extractedFolder = join(
-              dirname(__filename),
-              "ffmpeg-master-latest-linux64-gpl"
-            );
-            const newFolderName = join(dirname(__filename), "ffmpeg");
-            renameSync(extractedFolder, newFolderName);
-          } else throw new Error("@error: ffmpeg extraction failed");
-        });
-        return;
-      default:
-        let url =
-          "https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-";
-        let dSize = 0;
-        const arch = process.arch;
-        const platform = process.platform;
-        if (platform === "linux" && arch === "x64") url += "linux64-gpl.tar.xz";
-        else if (platform === "linux" && arch === "arm64") {
-          url += "linuxarm64-gpl.tar.xz";
-        } else throw new Error("Unsupported platform or architecture");
-        const response = await fetch(url);
-        if (!response.ok) throw new Error("@error: " + response.statusText);
-        const tSize = parseInt(response.headers.get("content-length"), 10);
-        const writer = createWriteStream(filepath);
-        response.body.on("data", (chunk) => {
-          dSize += chunk.length;
-          const progress = Math.round((dSize / tSize) * 100);
-          process.stdout.write(`@download progress: ${progress}%\r`);
-        });
-        await new Promise((resolve, reject) => {
-          response.body.pipe(writer);
-          writer.on("finish", resolve);
-          writer.on("error", reject);
-        });
-        extractProcess = spawn("tar", [
-          "xf",
-          filepath,
-          "-C",
-          dirname(__filename),
-        ]);
-        extractProcess.on("exit", (code) => {
-          if (code === 0) {
-            const extractedFolder = join(
-              dirname(__filename),
-              "ffmpeg-master-latest-linux64-gpl"
-            );
-            const newFolderName = join(dirname(__filename), "ffmpeg");
-            renameSync(extractedFolder, newFolderName);
-          } else throw new Error("@error: ffmpeg extraction failed");
-        });
-    }
+    const destinationPath = join(dirname(__filename), "engine");
+    const url =
+      "https://github.com/yt-dlp/yt-dlp-nightly-builds/releases/download/2024.02.29.232658/yt-dlp_linux";
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`@error: ${response.statusText}`);
+    const writer = createWriteStream(destinationPath);
+    response.body.pipe(writer);
+    writer.on("finish", () => {
+      console.log("@success: Download completed");
+    });
+    writer.on("error", (err) => {
+      throw new Error(`@error: ${err.message}`);
+    });
   } catch (error) {
-    console.error("@error:", error.message);
+    console.error(error.message);
   }
 };
 
-downloadAndExtract();
+downloadAndRename();
