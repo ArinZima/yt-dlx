ebfe1e94c479a6b09bdc11b55cfb7504c49b198f
yt-dlx <yt-dlx@proton.me>
2024-03-03 02:09:39 +0530

    yt-dlx @4.4.0

---
+++ shared/cli/npm.mjs
@@ -0,0 +1,17 @@
+import { exec } from 'child_process';
+
+// package.json
+var version = "4.3.0";
+exec("npm show yt-dlx version", (_error, stdout) => {
+  let logger = "";
+  const latestVersion = stdout.trim();
+  const currentVersionNum = version.split(".").map(Number).join("");
+  const latestVersionNum = latestVersion.split(".").map(Number).join("");
+  if (latestVersionNum > currentVersionNum) {
+    console.clear();
+    logger += `\x1B[31mUsing outdated version of yt-dlx@${version}
+`;
+    logger += `\x1B[31mPlease update to the latest version yt-dlx@${latestVersion}\x1B[0m`;
+    console.error(logger);
+  }
+});
