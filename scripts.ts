5df18451afb70a99e5f870681551030008545c85
yt-dlx <yt-dlx@proton.me>
2024-03-03 02:49:17 +0530

    yt-dlx @beta

--- scripts.ts
+++ scripts.ts
@@ -3,15 +3,15 @@ const scripts: object[] = [
     start: `node util/dev.mjs`,
     server: `node util/server.mjs`,
     remake: `yarn clean && yarn make && yarn update && yarn build`,
-    postinstall: `node util/ffmpeg.mjs && node util/engine.mjs && yarn permissions`,
+    postinstall: `node util/ffmpeg.mjs && node util/engine.mjs && chmod -R +x util/*`,
     prepublishOnly: `yarn remake && rm -rf util/ffmpeg util/ffmpeg.tar.xz util/engine`,
-    permissions: `chmod +x util/ffmpeg/bin/ffmpeg util/ffmpeg/bin/ffprobe util/ffmpeg/bin/ffplay util/engine`,
     upload: `yarn remake && yarn test && npm pkg fix && npm publish --access=public && yarn update`,
     spec: `tsup --config tsup.config.ts ./core/__tests__/quick.spec.ts --outDir .temp --clean && node .temp/quick.spec.js`,
     clean: {
-      main: `yarn clean:base && yarn clean:client && rm -rf util/ffmpeg util/ffmpeg.tar.xz util/engine`,
+      main: `yarn clean:base && yarn clean:client`,
       base: `rm -rf node_modules .temp shared others`,
       client: `cd client && rm -rf node_modules .next`,
+      deps: `rm -rf util/ffmpeg.tar.xz util/ffmpeg util/engine`,
     },
     make: {
       main: `yarn make:deps && yarn make:base && yarn make:client`,
