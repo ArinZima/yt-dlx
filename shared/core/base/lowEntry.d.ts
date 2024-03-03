0fe3fb254872eeb5645019f6ee4590afaed2f5ba
yt-dlx <yt-dlx@proton.me>
2024-03-03 20:52:48 +0530

    yt-dlx @beta

---
+++ shared/core/base/lowEntry.d.ts
@@ -0,0 +1,3 @@
+import type TubeConfig from "../interface/TubeConfig";
+export default function lowEntry(metaBody: TubeConfig[]): Promise<TubeConfig | undefined>;
+//# sourceMappingURL=lowEntry.d.ts.map
