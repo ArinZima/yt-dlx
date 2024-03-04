fe1c9bd9d1fe675417472fb02ebe050718844d97
yt-dlx <yt-dlx@proton.me>
2024-03-04 11:43:44 +0530

    yt-dlx @beta

--- core/__tests__/quick.spec.ts
+++ core/__tests__/quick.spec.ts
@@ -1,44 +1,66 @@
 console.clear();
 import proTube from "..";
+import async from "async";
 
-(async () => {
-  // =================================[AUDIO-ONLY]=================================
-  await proTube().audio().single().highest({
-    query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
-    torproxy: "socks5://127.0.0.1:9050",
-    output: "public/audio",
-    verbose: false,
-  });
-  await proTube().audio().single().lowest({
-    query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
-    torproxy: "socks5://127.0.0.1:9050",
-    output: "public/audio",
-    verbose: false,
-  });
-  // =================================[VIDEO-ONLY]=================================
-  await proTube().video().single().highest({
-    query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
-    torproxy: "socks5://127.0.0.1:9050",
-    output: "public/video",
-    verbose: false,
-  });
-  await proTube().video().single().lowest({
-    query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
-    torproxy: "socks5://127.0.0.1:9050",
-    output: "public/video",
-    verbose: false,
-  });
-  // =================================[AUDIO_VIDEO]=================================
-  await proTube().audio_video().single().highest({
-    query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
-    torproxy: "socks5://127.0.0.1:9050",
-    output: "public/mix",
-    verbose: false,
-  });
-  await proTube().audio_video().single().lowest({
-    query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
-    torproxy: "socks5://127.0.0.1:9050",
-    output: "public/mix",
-    verbose: false,
-  });
-})();
+async.series(
+  [
+    // =================================[AUDIO-ONLY]=================================
+    async () => {
+      await proTube().audio().single().highest({
+        query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+        torproxy: "socks5://127.0.0.1:9050",
+        output: "public/audio",
+        verbose: false,
+        stream: false,
+      });
+    },
+    async () => {
+      await proTube().audio().single().lowest({
+        query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+        torproxy: "socks5://127.0.0.1:9050",
+        output: "public/audio",
+        verbose: false,
+        stream: false,
+      });
+    },
+    // =================================[VIDEO-ONLY]=================================
+    async () => {
+      await proTube().video().single().highest({
+        query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+        torproxy: "socks5://127.0.0.1:9050",
+        output: "public/video",
+        verbose: false,
+        stream: false,
+      });
+    },
+    async () => {
+      await proTube().video().single().lowest({
+        query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+        torproxy: "socks5://127.0.0.1:9050",
+        output: "public/video",
+        verbose: false,
+        stream: false,
+      });
+    },
+    // =================================[AUDIO_VIDEO]=================================
+    async () => {
+      await proTube().audio_video().single().highest({
+        query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+        torproxy: "socks5://127.0.0.1:9050",
+        output: "public/mix",
+        verbose: false,
+        stream: false,
+      });
+    },
+    async () => {
+      await proTube().audio_video().single().lowest({
+        query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+        torproxy: "socks5://127.0.0.1:9050",
+        output: "public/mix",
+        verbose: false,
+        stream: false,
+      });
+    },
+  ],
+  (err) => console.error("@error:", err)
+);
--- shared/core/pipes/audio/ListAudioLowest.d.ts
+++
@@ -1,7 +0,0 @@
-export default function ListAudioLowest(input: {
-    query: string;
-    output?: string;
-    verbose?: boolean;
-    filter?: "echo" | "slow" | "speed" | "phaser" | "flanger" | "panning" | "reverse" | "vibrato" | "subboost" | "surround" | "bassboost" | "nightcore" | "superslow" | "vaporwave" | "superspeed";
-}): Promise<void>;
-//# sourceMappingURL=ListAudioLowest.d.ts.map
