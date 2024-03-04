c5e19a418df66397171c6ac33f69c97db1568cea
yt-dlx <yt-dlx@proton.me>
2024-03-04 13:28:21 +0530

    yt-dlx @beta

---
+++ shared/core/base/Engine.d.ts
@@ -0,0 +1,6 @@
+import type EngineResult from "../interface/EngineResult";
+export default function Engine({ query, torproxy, }: {
+    query: string;
+    torproxy?: string;
+}): Promise<EngineResult>;
+//# sourceMappingURL=Engine.d.ts.map
