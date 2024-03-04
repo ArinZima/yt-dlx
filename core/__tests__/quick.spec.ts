241bf348015fcafa95c18403c74d1e6ea3ec6801
yt-dlx <yt-dlx@proton.me>
2024-03-04 14:29:50 +0530

    yt-dlx @beta

--- core/__tests__/quick.spec.ts
+++ core/__tests__/quick.spec.ts
@@ -1,23 +1,2 @@
 console.clear();
-import ytdlx from "..";
-
-const options = {
-  stream: false,
-  verbose: false,
-  output: "public",
-  // torproxy: "socks5://127.0.0.1:9050",
-  query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
-};
-
-(async () => {
-  try {
-    // await ytdlx.audio.single.lowest(options); // [AUDIO-ONLY]
-    // await ytdlx.audio.single.highest(options); // [AUDIO-ONLY]
-    // await ytdlx.video.single.lowest(options); // [VIDEO-ONLY]
-    await ytdlx.video.single.highest(options); // [VIDEO-ONLY]
-    // await ytdlx.audio_video.single.lowest(options); // [AUDIO_VIDEO]
-    // await ytdlx.audio_video.single.highest(options); // [AUDIO_VIDEO]
-  } catch (error) {
-    console.error(error);
-  }
-})();
+// torproxy: "socks5://127.0.0.1:9050",
