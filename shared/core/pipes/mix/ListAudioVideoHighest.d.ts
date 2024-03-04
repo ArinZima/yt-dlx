631299c0c58fa48795ff2260720aa8c3a4d88f4b
yt-dlx <yt-dlx@proton.me>
2024-03-04 09:32:06 +0530

    yt-dlx @beta

---
+++ shared/core/pipes/mix/ListAudioVideoHighest.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="fluent-ffmpeg" />
+import type { gpuffmpegCommand } from "../../base/ffmpeg";
+export default function ListAudioVideoHighest(input: {
+    query: string;
+    output?: string;
+    verbose?: boolean;
+    filter?: "invert" | "rotate90" | "rotate270" | "grayscale" | "rotate180" | "flipVertical" | "flipHorizontal";
+}): Promise<void | {
+    filename: string;
+    ffmpeg: gpuffmpegCommand;
+}>;
+//# sourceMappingURL=ListAudioVideoHighest.d.ts.map
---
+++ shared/core/pipes/mix/ListAudioVideoQualityCustom.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="fluent-ffmpeg" />
+import type { gpuffmpegCommand } from "../../base/ffmpeg";
+export default function ListAudioVideoQualityCustom(input: {
+    query: string;
+    output?: string;
+    verbose?: boolean;
+    AQuality: "high" | "medium" | "low" | "ultralow";
+    VQuality: "144p" | "240p" | "360p" | "480p" | "720p" | "1080p" | "1440p" | "2160p" | "2880p" | "4320p" | "5760p" | "8640p" | "12000p";
+    filter?: "invert" | "rotate90" | "rotate270" | "grayscale" | "rotate180" | "flipVertical" | "flipHorizontal";
+}): Promise<void | {
+    filename: string;
+    ffmpeg: gpuffmpegCommand;
+}>;
+//# sourceMappingURL=ListAudioVideoQualityCustom.d.ts.map
