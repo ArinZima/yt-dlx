aaa1a826a293a95d1c15d011d2eb0c8507de39a8
yt-dlx <yt-dlx@proton.me>
2024-03-04 11:05:00 +0530

    yt-dlx @beta

--- core/__tests__/quick.spec.ts
+++ core/__tests__/quick.spec.ts
@@ -2,9 +2,44 @@ console.clear();
 import ytDlx from "..";
 
 (async () => {
-  await ytDlx.video.single.highest({
+  await ytDlx().audio().single().highest({
     query: "https://www.youtube.com/watch?v=7PIji8OubXU",
+    torproxy: "socks5://127.0.0.1:9050",
+    verbose: false,
+    output: "temp",
+  });
+  await ytDlx().audio().single().lowest({
+    query: "https://www.youtube.com/watch?v=7PIji8OubXU",
+    torproxy: "socks5://127.0.0.1:9050",
+    verbose: false,
+    output: "temp",
+  });
+
+  await ytDlx().video().single().highest({
+    query: "https://www.youtube.com/watch?v=7PIji8OubXU",
+    torproxy: "socks5://127.0.0.1:9050",
+    verbose: false,
+    output: "temp",
+  });
+  await ytDlx().video().single().lowest({
+    query: "https://www.youtube.com/watch?v=7PIji8OubXU",
+    torproxy: "socks5://127.0.0.1:9050",
+    verbose: false,
+    output: "temp",
+  });
+
+  await ytDlx().audio_video().single().highest({
+    query: "https://www.youtube.com/watch?v=7PIji8OubXU",
+    torproxy: "socks5://127.0.0.1:9050",
+    verbose: false,
+    output: "temp",
+  });
+  await ytDlx().audio_video().single().lowest({
+    query: "https://www.youtube.com/watch?v=7PIji8OubXU",
+    torproxy: "socks5://127.0.0.1:9050",
     verbose: false,
     output: "temp",
   });
 })();
+
+// torproxy: "socks5://127.0.0.1:9050",
--- core/index.ts
+++ core/index.ts
@@ -9,56 +9,125 @@ import AudioHighest from "./pipes/audio/AudioHighest";
 import AudioQualityCustom from "./pipes/audio/AudioQualityCustom";
 import ListAudioLowest from "./pipes/audio/ListAudioLowest";
 import ListAudioHighest from "./pipes/audio/ListAudioHighest";
+import ListAudioQualityCustom from "./pipes/audio/ListAudioQualityCustom";
 
 import VideoLowest from "./pipes/video/VideoLowest";
 import VideoHighest from "./pipes/video/VideoHighest";
 import VideoQualityCustom from "./pipes/video/VideoQualityCustom";
 import ListVideoLowest from "./pipes/video/ListVideoLowest";
 import ListVideoHighest from "./pipes/video/ListVideoHighest";
+import ListVideoQualityCustom from "./pipes/video/ListVideoQualityCustom";
 
 import AudioVideoLowest from "./pipes/mix/AudioVideoLowest";
 import AudioVideoHighest from "./pipes/mix/AudioVideoHighest";
 import AudioVideoQualityCustom from "./pipes/mix/AudioVideoQualityCustom.";
+import ListAudioVideoHighest from "./pipes/mix/ListAudioVideoHighest";
+import ListAudioVideoLowest from "./pipes/mix/ListAudioVideoLowest";
+import ListAudioVideoQualityCustom from "./pipes/mix/ListAudioVideoQualityCustom";
 
-const ytdlx = {
-  search: {
-    PlaylistInfo: web.search.PlaylistInfo,
-    SearchVideos: web.search.SearchVideos,
-    VideoInfo: web.search.VideoInfo,
-  },
-  info: {
-    help,
-    extract,
-    list_formats,
-    extract_playlist_videos,
-  },
-  audio: {
-    single: {
-      lowest: AudioLowest,
-      highest: AudioHighest,
-      custom: AudioQualityCustom,
-    },
-    playlist: {
-      lowest: ListAudioLowest,
-      highest: ListAudioHighest,
-    },
-  },
-  video: {
-    single: {
-      lowest: VideoLowest,
-      highest: VideoHighest,
-      custom: VideoQualityCustom,
-    },
-    playlist: {
-      lowest: ListVideoLowest,
-      highest: ListVideoHighest,
-    },
-  },
-  audio_video: {
-    lowest: AudioVideoLowest,
-    highest: AudioVideoHighest,
-    custom: AudioVideoQualityCustom,
-  },
+// const ytdlx = {
+// search: {
+// PlaylistInfo: web.search.PlaylistInfo,
+// SearchVideos: web.search.SearchVideos,
+// VideoInfo: web.search.VideoInfo,
+// },
+// info: {
+// help,
+// extract,
+// list_formats,
+// extract_playlist_videos,
+// },
+// audio: {
+// single: {
+// lowest: AudioLowest,
+// highest: AudioHighest,
+// custom: AudioQualityCustom,
+// },
+// playlist: {
+// lowest: ListAudioLowest,
+// highest: ListAudioHighest,
+// custom: ListAudioQualityCustom,
+// },
+// },
+// video: {
+// single: {
+// lowest: VideoLowest,
+// highest: VideoHighest,
+// custom: VideoQualityCustom,
+// },
+// playlist: {
+// lowest: ListVideoLowest,
+// highest: ListVideoHighest,
+// custom: ListVideoQualityCustom,
+// },
+// },
+// audio_video: {
+// single: {
+// lowest: AudioVideoLowest,
+// highest: AudioVideoHighest,
+// custom: AudioVideoQualityCustom,
+// },
+// playlist: {
+// lowest: ListAudioVideoHighest,
+// highest: ListAudioVideoLowest,
+// custom: ListAudioVideoQualityCustom,
+// },
+// },
+// };
+
+// export default ytdlx;
+
+const ytdlx = () => {
+  const ytDlx = {
+    search: () => ({
+      PlaylistInfo: web.search.PlaylistInfo,
+      SearchVideos: web.search.SearchVideos,
+      VideoInfo: web.search.VideoInfo,
+    }),
+    info: () => ({
+      help,
+      extract,
+      list_formats,
+      extract_playlist_videos,
+    }),
+    audio: () => ({
+      single: () => ({
+        lowest: AudioLowest,
+        highest: AudioHighest,
+        custom: AudioQualityCustom,
+      }),
+      playlist: () => ({
+        lowest: ListAudioLowest,
+        highest: ListAudioHighest,
+        custom: ListAudioQualityCustom,
+      }),
+    }),
+    video: () => ({
+      single: () => ({
+        lowest: VideoLowest,
+        highest: VideoHighest,
+        custom: VideoQualityCustom,
+      }),
+      playlist: () => ({
+        lowest: ListVideoLowest,
+        highest: ListVideoHighest,
+        custom: ListVideoQualityCustom,
+      }),
+    }),
+    audio_video: () => ({
+      single: () => ({
+        lowest: AudioVideoLowest,
+        highest: AudioVideoHighest,
+        custom: AudioVideoQualityCustom,
+      }),
+      playlist: () => ({
+        lowest: ListAudioVideoHighest,
+        highest: ListAudioVideoLowest,
+        custom: ListAudioVideoQualityCustom,
+      }),
+    }),
+  };
+  return ytDlx;
 };
 
 export default ytdlx;
