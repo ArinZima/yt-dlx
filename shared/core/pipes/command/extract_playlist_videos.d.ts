868edffca083d963cb7938bab34f93a1a1afae0c
yt-dlx <yt-dlx@proton.me>
2024-03-05 16:09:54 +0530

    yt-dlx @beta

--- shared/core/pipes/command/extract_playlist_videos.d.ts
+++
@@ -1,6 +0,0 @@
-import type EngineResult from "../../interface/EngineResult";
-export default function extract_playlist_videos({ torproxy, playlistUrls, }: {
-    torproxy?: string;
-    playlistUrls: string[];
-}): Promise<EngineResult[]>;
-//# sourceMappingURL=extract_playlist_videos.d.ts.map
--- shared/core/web/crawler.d.ts
+++
@@ -1,5 +0,0 @@
-import { Browser, Page } from "puppeteer";
-export declare let browser: Browser;
-export declare let page: Page;
-export default function crawler(verbose?: boolean, torproxy?: string): Promise<void>;
-//# sourceMappingURL=crawler.d.ts.map
