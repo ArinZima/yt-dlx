4c235072045ba51ece18826fc215faa28876f6d3
yt-dlx <yt-dlx@proton.me>
2024-03-05 15:32:32 +0530

    yt-dlx @beta

--- core/__tests__/other/scrape.spec.ts
+++ core/__tests__/other/scrape.spec.ts
@@ -16,25 +16,29 @@ import type {
       | VideoInfoType
       | PlaylistInfoType;
     metaTube = (await web.search.SearchVideos({
-      screenshot: false,
       query: "PERSONAL BY PLAZA",
+      screenshot: false,
+      verbose: true,
       type: "video",
     })) as TypeVideo[];
     console.log(colors.green("@pass:"), "video search results received");
     metaTube = (await web.search.VideoInfo({
       query: metaTube[0]?.videoLink as string,
       screenshot: false,
+      verbose: true,
     })) as VideoInfoType;
     console.log(colors.green("@pass:"), "single video data received");
     metaTube = (await web.search.SearchVideos({
       query: metaTube.title,
       screenshot: false,
       type: "playlist",
+      verbose: true,
     })) as TypePlaylist[];
     console.log(colors.green("@pass:"), "playlist search results received");
     metaTube = (await web.search.PlaylistInfo({
       query: metaTube[0]?.playlistLink as string,
       screenshot: false,
+      verbose: true,
     })) as PlaylistInfoType;
     console.log(colors.green("@pass:"), "single playlist data received");
   } catch (error: any) {
--- core/web/crawler.ts
+++ core/web/crawler.ts
@@ -2,17 +2,19 @@ import puppeteer, { Browser, Page } from "puppeteer";
 export let browser: Browser;
 export let page: Page;
 
-export default async function crawler(verbose?: boolean) {
+export default async function crawler(verbose?: boolean, torprox?: string) {
   try {
+    if (torprox) console.log(torprox);
     browser = await puppeteer.launch({
       headless: verbose ? false : true,
-      // userDataDir: "others",
+      userDataDir: "others",
       args: [
         "--no-zygote",
         "--incognito",
         "--no-sandbox",
         "--enable-automation",
         "--disable-dev-shm-usage",
+        `--proxy-server=socks5://127.0.0.1:9050`,
       ],
     });
     page = await browser.newPage();
