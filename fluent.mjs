f64c8c1efce4a330d32cdc37a15a6a5d7f4a20ea
yt-dlx <ytdlx@protonmail.me>
2024-03-09 14:56:27 +0530

    yt-dlx @beta

--- fluent.mjs
+++ fluent.mjs
@@ -1,30 +1,33 @@
-import puppeteer from "puppeteer";
 import ffmpeg from "fluent-ffmpeg";
-
-async function proTube(videoUrl) {
+import puppeteer from "puppeteer";
+async function proTube({ videoUrl }) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(videoUrl);
   await page.waitForSelector("script");
-  const src = await page.evaluate(() => {
+  const metaTube = await page.evaluate(() => {
     const ytInitialPlayerResponse = window.ytInitialPlayerResponse;
     if (ytInitialPlayerResponse && ytInitialPlayerResponse.streamingData) {
       const streamingData = ytInitialPlayerResponse.streamingData;
       const formats = streamingData.formats || [];
-      const adaptiveFormats = streamingData.adaptiveFormats || [];
-      const allFormats = formats.concat(adaptiveFormats);
+      const allFormats = formats.concat(streamingData.adaptiveFormats || []);
       return allFormats.map((format) => format);
     } else return null;
   });
-  if (src) {
-    console.log(src);
-    const fluent = ffmpeg(src[0].url);
+  if (page) await page.close();
+  if (browser) await browser.close();
+  if (metaTube) return metaTube;
+  else return undefined;
+}
+
+(async () => {
+  const YouTube = await proTube({
+    videoUrl: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+  });
+  if (YouTube) {
+    const fluent = ffmpeg(YouTube[0].url);
     fluent.output("public/music.mkv");
     fluent.format("matroska");
     fluent.run();
-  } else console.log("@error: failed to extract URLs.");
-  if (page) await page.close();
-  if (browser) await browser.close();
-}
-
-proTube("https://www.youtube.com/watch?v=AbFnsaDQMYQ");
+  }
+})();
