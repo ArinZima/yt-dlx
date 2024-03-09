c28650d9a9eb6aa4ba35b921f0b2681ea1f935e2
yt-dlx <ytdlx@protonmail.me>
2024-03-09 16:19:11 +0530

    yt-dlx @beta

--- fluent.mjs
+++ fluent.mjs
@@ -1,11 +1,10 @@
-import * as async from "async";
 import puppeteer from "puppeteer";
 import ffmpeg from "fluent-ffmpeg";
 
 async function proTube({ videoUrl }) {
   const browser = await puppeteer.launch({
     userDataDir: "others",
-    headless: true,
+    headless: false,
     args: [
       "--no-zygote",
       "--incognito",
@@ -20,35 +19,56 @@ async function proTube({ videoUrl }) {
   );
   await page.goto(videoUrl);
   await page.waitForSelector("script");
+  const metaData = await page.evaluate(() => {
+    const titleElement = document.querySelector("h1.title");
+    const title = titleElement ? titleElement.textContent.trim() : undefined;
+    const descriptionElement = document.querySelector("#description");
+    const description = descriptionElement
+      ? descriptionElement.textContent.trim()
+      : undefined;
+    const viewsElement = document.querySelector(".view-count");
+    const views = viewsElement ? viewsElement.textContent.trim() : undefined;
+    const likesElement = document.querySelector(
+      ".like-button-renderer .like-button-renderer-like-button"
+    );
+    const likes = likesElement ? likesElement.textContent.trim() : undefined;
+    const dislikesElement = document.querySelector(
+      ".like-button-renderer .like-button-renderer-dislike-button"
+    );
+    const dislikes = dislikesElement
+      ? dislikesElement.textContent.trim()
+      : undefined;
+    return { title, description, views, likes, dislikes };
+  });
   const metaTube = await page.evaluate(() => {
     const ytInitialPlayerResponse = window.ytInitialPlayerResponse;
     if (ytInitialPlayerResponse && ytInitialPlayerResponse.streamingData) {
       const streamingData = ytInitialPlayerResponse.streamingData;
-      const formats = streamingData.formats || [];
-      const pops = formats.concat(streamingData.adaptiveFormats || []);
-      return pops.map((ipop) => ipop);
-    } else return null;
+      const AdaptFormats = streamingData.AdaptFormats || [];
+      const Formats = streamingData.formats || [];
+      const pops = Formats.concat(AdaptFormats);
+      const AudioStore = [];
+      const VideoStore = [];
+      pops.forEach((p) => {
+        if (p.mimeType.includes("audio")) AudioStore.push(p);
+        else if (p.mimeType.includes("video")) VideoStore.push(p);
+      });
+      return { AudioStore, VideoStore };
+    } else return undefined;
   });
   if (page) await page.close();
   if (browser) await browser.close();
-  if (metaTube) return metaTube;
-  else return undefined;
+  if (metaTube & metaData) return { metaTube, metaData };
+  else throw new Error("Error fetching video data");
 }
 
-async.waterfall([
-  async () => {
-    const metaTube = await proTube({
-      videoUrl: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
-    });
-    return metaTube;
-  },
-  async (metaTube) => {
-    const fluent = ffmpeg(metaTube[0].url);
-    return fluent;
-  },
-  async (fluent) => {
-    fluent.output("temp/music.mkv");
-    fluent.format("matroska");
-    fluent.run();
-  },
-]);
+(async () => {
+  const { metaTube, metaData } = await proTube({
+    videoUrl: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+  });
+  console.log({ metaTube, metaData });
+  const fluent = ffmpeg(metaTube[0].url);
+  fluent.output("temp/music.mkv");
+  fluent.format("matroska");
+  fluent.run();
+})();
