f0dbd44a44c06d96fe9b5422fbf46652bbf7250a
yt-dlx <yt-dlx@proton.me>
2024-03-10 14:35:08 +0530

    yt-dlx @beta

---
+++ shared/core/pipes/audio/list/ListAudioLowest.d.ts
@@ -0,0 +1,8 @@
+export default function ListAudioLowest(input: {
+    query: string[];
+    output?: string;
+    verbose?: boolean;
+    torproxy?: string;
+    filter?: "echo" | "slow" | "speed" | "phaser" | "flanger" | "panning" | "reverse" | "vibrato" | "subboost" | "surround" | "bassboost" | "nightcore" | "superslow" | "vaporwave" | "superspeed";
+}): Promise<void>;
+//# sourceMappingURL=ListAudioLowest.d.ts.map
