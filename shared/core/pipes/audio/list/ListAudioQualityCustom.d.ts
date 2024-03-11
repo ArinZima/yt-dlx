2f77ee80a38d37953c192ae1b51a27df1983c12e
yt-dlx <yt-dlx@proton.me>
2024-03-11 20:58:24 +0530

    yt-dlx @beta

---
+++ shared/core/pipes/audio/list/ListAudioQualityCustom.d.ts
@@ -0,0 +1,9 @@
+export default function ListAudioQualityCustom(input: {
+    query: string[];
+    output?: string;
+    verbose?: boolean;
+    autoSocks5?: boolean;
+    quality: "high" | "medium" | "low" | "ultralow";
+    filter?: "echo" | "slow" | "speed" | "phaser" | "flanger" | "panning" | "reverse" | "vibrato" | "subboost" | "surround" | "bassboost" | "nightcore" | "superslow" | "vaporwave" | "superspeed";
+}): Promise<void>;
+//# sourceMappingURL=ListAudioQualityCustom.d.ts.map
