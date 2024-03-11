e2ad8f81e3dc63b20a872d583d54441e142dafa7
yt-dlx <yt-dlx@proton.me>
2024-03-11 12:01:14 +0530

    yt-dlx @beta

---
+++ shared/core/pipes/mix/list/ListAudioVideoQualityCustom.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="fluent-ffmpeg" />
+import type { proTubeCommand } from "../../../base/ffmpeg";
+export default function ListAudioVideoQualityCustom(input: {
+    query: string[];
+    output?: string;
+    verbose?: boolean;
+    autoSocks5?: boolean;
+    AQuality: "high" | "medium" | "low" | "ultralow";
+    VQuality: "144p" | "240p" | "360p" | "480p" | "720p" | "1080p" | "1440p" | "2160p" | "2880p" | "4320p" | "5760p" | "8640p" | "12000p";
+    filter?: "invert" | "rotate90" | "rotate270" | "grayscale" | "rotate180" | "flipVertical" | "flipHorizontal";
+}): Promise<void | {
+    filename: string;
+    ffmpeg: proTubeCommand;
+}>;
+//# sourceMappingURL=ListAudioVideoQualityCustom.d.ts.map
