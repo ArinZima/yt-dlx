f02a8437dea3d8145bd2bd2a7f11c0eca9e10fe7
yt-dlx <yt-dlx@proton.me>
2024-03-11 18:25:32 +0530

    yt-dlx @beta

--- util/socks5.sh
+++ util/socks5.sh
@@ -1,50 +1,51 @@
 #!/bin/bash
-
 NC='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 ERR() {
-    local message="$1"
-    echo -e "${RED}ERROR:${NC} $message"
+    echo -e "${RED}ERROR: $1${NC}"
 }
 LOG() {
-    local message="$1"
-    echo -e "${GREEN}INFO:${NC} $message"
+    echo -e "${GREEN}INFO: $1${NC}"
 }
 RUN() {
     local command="$1"
-    local error_message="$2"
     if [ "$use_sudo" = true ]; then
-        command="sudo $command"
+        command="sudo sh -c '$command'"
+    else
+        command="sh -c '$command'"
     fi
     if ! eval "$command"; then
-        ERR "$error_message"
+        ERR "Failed to execute command: $command"
     fi
 }
 if [ -x "$(command -v sudo)" ]; then
+    LOG "Using sudo command"
     use_sudo=true
 else
     use_sudo=false
 fi
 if [ -x "$(command -v apt-get)" ]; then
     LOG "Detected Debian-based system"
-    RUN "rm -rf ~/.nyx" "Failed to remove Nyx config"
-    RUN "rm -rf /etc/tor/torrc" "Failed to remove Tor config"
-    RUN "echo -e 'ControlPort 9051\nCookieAuthentication 1\nCookieAuthFileGroupReadable 1\nMaxCircuitDirtiness 60' | sudo tee /etc/tor/torrc > /dev/null" "Failed to create default torrc file"
-    RUN "apt update && sudo apt install -y tor nyx" "Failed to update and install Tor and Nyx"
-    RUN "systemctl enable --now tor" "Failed to enable and start Tor service"
-    RUN 'sh -c "echo -e \"redraw_rate 60\nwrite_logs_to /var/log/nyx/notices.log\" > ~/.nyx/config"' "Failed to configure Nyx"
-    RUN 'curl --socks5-hostname 127.0.0.1:9050 https://checkip.amazonaws.com' "Failed to connect to Tor via Nyx"
+    RUN "systemctl stop tor"
+    RUN "systemctl daemon-reload"
+    RUN "apt-get purge -y tor nyx"
+    RUN "apt-get update && apt-get install -y tor nyx"
+    RUN "echo -e 'ControlPort 9051\nCookieAuthentication 1\nCookieAuthFileGroupReadable 1' > /etc/tor/torrc"
+    RUN "echo -e 'redraw_rate 60\nwrite_logs_to /var/log/nyx/notices.log' > /root/.nyx/config"
+    RUN "systemctl enable --now tor"
+    RUN "curl --socks5-hostname 127.0.0.1:9050 https://checkip.amazonaws.com"
     elif [ -x "$(command -v pacman)" ]; then
+    RUN "systemctl stop tor"
+    RUN "systemctl daemon-reload"
     LOG "Detected Arch-based system"
-    RUN "rm -rf ~/.nyx" "Failed to remove Nyx config"
-    RUN "rm -rf /etc/tor/torrc" "Failed to remove Tor config"
-    RUN "echo -e 'ControlPort 9051\nCookieAuthentication 1\nCookieAuthFileGroupReadable 1\nMaxCircuitDirtiness 60' | sudo tee /etc/tor/torrc > /dev/null" "Failed to create default torrc file"
-    RUN "pacman -Syyu --noconfirm tor nyx" "Failed to update and install Tor and Nyx"
-    RUN "systemctl enable --now tor" "Failed to enable and start Tor service"
-    RUN 'sh -c "echo -e \"redraw_rate 60\nwrite_logs_to /var/log/nyx/notices.log\" > ~/.nyx/config"' "Failed to configure Nyx"
-    RUN 'curl --socks5-hostname 127.0.0.1:9050 https://checkip.amazonaws.com' "Failed to connect to Tor via Nyx"
+    RUN "pacman -Rns --noconfirm tor nyx"
+    RUN "pacman -Syyu --noconfirm tor nyx"
+    RUN "echo -e 'ControlPort 9051\nCookieAuthentication 1\nCookieAuthFileGroupReadable 1' > /etc/tor/torrc"
+    RUN "echo -e 'redraw_rate 60\nwrite_logs_to /var/log/nyx/notices.log' > /root/.nyx/config"
+    RUN "systemctl enable --now tor"
+    RUN "curl --socks5-hostname 127.0.0.1:9050 https://checkip.amazonaws.com"
 else
     ERR "Neither apt-get nor pacman found. Unsupported system."
 fi
