cbfb41f6b7eed2b65e72b20c34bf9acdfa1072b5
yt-dlx <yt-dlx@proton.me>
2024-03-11 09:16:02 +0530

    yt-dlx @beta

--- core/__tests__/other/quick.spec.ts
+++ core/__tests__/other/quick.spec.ts
@@ -1,1 +1,17 @@
 console.clear();
+import ytdlx from "../..";
+import colors from "colors";
+
+(async () => {
+  try {
+    await ytdlx().AudioOnly().Single().Highest({
+      stream: false,
+      verbose: false,
+      output: "public/audio",
+      proxy: "socks5://127.0.0.1:9050",
+      query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+    });
+  } catch (error: any) {
+    console.error(colors.red(error.message));
+  }
+})();
