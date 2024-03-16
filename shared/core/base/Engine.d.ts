cfa769ef9f624543122bb605f935af52e8accee6
yt-dlx <yt-dlx@proton.me>
2024-03-16 18:21:57 +0530

    yt-dlx 5.18.0

--- shared/core/base/Engine.d.ts
+++
@@ -1,8 +0,0 @@
-import type EngineResult from "../interface/EngineResult";
-export declare function sizeFormat(filesize: number): string | number;
-export default function Engine({ query, ipAddress, autoSocks5, }: {
-    query: string;
-    ipAddress: string;
-    autoSocks5?: boolean;
-}): Promise<EngineResult>;
-//# sourceMappingURL=Engine.d.ts.map
