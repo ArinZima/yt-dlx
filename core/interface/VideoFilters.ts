5e8ddd25d1d8e885aa1c246ae7927f9e20b9b8c2
yt-dlx <yt-dlx@proton.me>
2024-03-17 21:26:03 +0530

    yt-dlx 5.18.0
    yt-dlx 5.18.0

--- core/interface/VideoFilters.ts
+++
@@ -1,9 +0,0 @@
-export default interface VideoFilters {
-  grayscale: string;
-  invert: string;
-  rotate90: string;
-  rotate180: string;
-  rotate270: string;
-  flipHorizontal: string;
-  flipVertical: string;
-}
