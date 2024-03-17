b5d73ff3d8c555fc6b2c4cb6595470f034917a3b
yt-dlx <yt-dlx@proton.me>
2024-03-17 11:12:01 +0530

    yt-dlx 5.18.0

--- core/base/ffmpeg.ts
+++ core/base/ffmpeg.ts
@@ -4,38 +4,40 @@ import ffmpeg from "fluent-ffmpeg";
 import { progressBar } from "./progressBar";
 import type { FfmpegCommand as proTubeCommand } from "fluent-ffmpeg";
 
+interface ProTubeOptions {
+  adata?: string;
+  vdata?: string;
+  ipAddress: string;
+}
+
 export default async function proTube({
   adata,
   vdata,
   ipAddress,
-}: {
-  adata?: any;
-  vdata?: any;
-  ipAddress: string;
-}): Promise<proTubeCommand> {
-  const ff: proTubeCommand = ffmpeg();
-  if (vdata && !adata) ff.addInput(vdata.AVDownload.mediaurl);
-  else if (adata && !vdata) ff.addInput(adata.AVDownload.mediaurl);
-  else if (adata && vdata) {
-    ff.addInput(vdata.AVDownload.mediaurl);
-    ff.addInput(adata.AVDownload.mediaurl);
-    ff.withOutputOptions(["-map 0:v:0", "-map 1:a:0"]);
-  }
-  console.log(
-    colors.green("@ffmpeg:"),
-    "using",
-    colors.green("ipAddress"),
-    ipAddress
-  );
-  const numCores = os.cpus().length;
-  const numThreads = numCores * 2;
-  ff.outputOptions("-c copy");
-  ff.addOption("-threads", numThreads.toString());
-  ff.addOption("-headers", `X-Forwarded-For: ${ipAddress}`);
-  ff.on("progress", (progress) => progressBar(progress));
-  ff.on("end", () => process.stdout.write("\n"));
-  ff.on("error", (error) => {
-    throw new Error(error.message);
+}: ProTubeOptions): Promise<proTubeCommand> {
+  return new Promise(async (resolve, reject) => {
+    const numThreads = os.cpus().length * 2;
+    const ff: proTubeCommand = ffmpeg();
+    ff.outputFormat("matroska");
+    ff.outputOptions("-c copy");
+    ff.addOption("-threads", numThreads.toString());
+    ff.addOption("-headers", "X-Forwarded-For: " + ipAddress);
+    if (vdata && !adata) ff.addInput(vdata);
+    if (adata && !vdata) ff.addInput(adata);
+    if (adata && vdata) {
+      ff.addInput(vdata);
+      ff.addInput(adata);
+      ff.withOutputOptions(["-map 0:v:0", "-map 1:a:0"]);
+    }
+    console.log(
+      colors.green("@info:"),
+      "ffmpeg using",
+      colors.green("ipAddress"),
+      ipAddress
+    );
+    ff.on("end", () => process.stdout.write("\n"));
+    ff.on("error", (error) => reject(error.message));
+    ff.on("progress", (progress) => progressBar(progress));
+    resolve(ff);
   });
-  return ff;
 }
