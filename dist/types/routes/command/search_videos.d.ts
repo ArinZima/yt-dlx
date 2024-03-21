6f8ee3870b41854085c5ece95a1a74a3b8c88df5
yt-dlx <yt-dlx@proton.me>
2024-03-21 23:05:08 +0530

    yt-dlx 6.8.0

--- dist/types/routes/command/search_videos.d.ts
+++
@@ -1,6 +0,0 @@
-import { searchVideosType } from "../../web";
-interface ipop {
-    query: string;
-}
-export default function search_videos({ query, }: ipop): Promise<searchVideosType>;
-export {};
