378d685a9fcf3d420a772300947adca98ddd4f91
yt-dlx <yt-dlx@proton.me>
2024-03-21 15:49:21 +0530

    yt-dlx 6.7.0

--- dist/cjs/core/web/crawler.js
+++ dist/cjs/core/web/crawler.js
@@ -16,39 +16,20 @@ exports.page = exports.browser = void 0;
 const puppeteer_1 = __importDefault(require("puppeteer"));
 function crawler(verbose, onionTor) {
     return __awaiter(this, void 0, void 0, function* () {
-        if (onionTor) {
-            exports.browser = yield puppeteer_1.default.launch({
-                headless: verbose ? false : true,
-                ignoreHTTPSErrors: true,
-                args: [
-                    "--no-zygote",
-                    "--incognito",
-                    "--no-sandbox",
-                    "--lang=en-US",
-                    "--enable-automation",
-                    "--disable-dev-shm-usage",
-                    "--ignore-certificate-errors",
-                    "--allow-running-insecure-content",
-                    "--proxy-server=socks5://127.0.0.1:9050",
-                ],
-            });
-        }
-        else {
-            exports.browser = yield puppeteer_1.default.launch({
-                headless: verbose ? false : true,
-                ignoreHTTPSErrors: true,
-                args: [
-                    "--no-zygote",
-                    "--incognito",
-                    "--no-sandbox",
-                    "--lang=en-US",
-                    "--enable-automation",
-                    "--disable-dev-shm-usage",
-                    "--ignore-certificate-errors",
-                    "--allow-running-insecure-content",
-                ],
-            });
-        }
+        exports.browser = yield puppeteer_1.default.launch({
+            headless: verbose ? false : true,
+            ignoreHTTPSErrors: true,
+            args: [
+                "--no-zygote",
+                "--incognito",
+                "--no-sandbox",
+                "--lang=en-US",
+                "--enable-automation",
+                "--disable-dev-shm-usage",
+                "--ignore-certificate-errors",
+                "--allow-running-insecure-content",
+            ],
+        });
         exports.page = yield exports.browser.newPage();
         yield exports.page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36");
     });
--- dist/types/routes/Audio/list/ListAudioLowest.d.ts
+++
@@ -1,7 +0,0 @@
-export default function ListAudioLowest(input: {
-    query: string[];
-    output?: string;
-    verbose?: boolean;
-    onionTor?: boolean;
-    filter?: "echo" | "slow" | "speed" | "phaser" | "flanger" | "panning" | "reverse" | "vibrato" | "subboost" | "surround" | "bassboost" | "nightcore" | "superslow" | "vaporwave" | "superspeed";
-}): Promise<void>;
