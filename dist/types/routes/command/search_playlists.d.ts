88ebf5f8ea4c0dc8f83ebb4a2a6cbe1c9553985c
yt-dlx <yt-dlx@proton.me>
2024-03-21 15:44:30 +0530

    yt-dlx 6.7.0

--- dist/types/routes/command/search_playlists.d.ts
+++
@@ -1,6 +0,0 @@
-import { searchPlaylistsType } from "../../web";
-interface ipop {
-    query: string;
-}
-export default function search_playlists({ query, }: ipop): Promise<searchPlaylistsType>;
-export {};
--- dist/types/web/vercel/playlistVideos.d.ts
+++
@@ -1,9 +0,0 @@
-export interface playlistVideosType {
-    id: string;
-    title: string;
-    videoCount: number;
-    thumbnails: string[];
-}
-export default function playlistVideos({ playlistId, }: {
-    playlistId: string;
-}): Promise<any>;
