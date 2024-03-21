5cf5a3d6ecab51b7f10857a838b0a6be91f53104
yt-dlx <yt-dlx@proton.me>
2024-03-21 16:00:00 +0530

    yt-dlx 6.7.0

--- util/wakeLock.js
+++ util/wakeLock.js
@@ -7,46 +7,6 @@ const app = express();
 const port = process.env.PORT || 8000;
 app.use((req, res) => res.status(404).send("404!"));
 
-app.get("/", async (req, res) => {
-  const query = req.query.query;
-  if (!query) {
-    return res.status(400).json({ error: "Query parameter is required" });
-  } else {
-    try {
-      const eproc = spawn("util/engine", ["--dump-json", `ytsearch:${query}`]);
-      const ipProcess = spawn("curl", ["ipinfo.io/ip"]);
-      let result = "";
-      let pubip = "";
-      eproc.stdout.on("data", (data) => {
-        result += data.toString();
-      });
-      ipProcess.stdout.on("data", (data) => {
-        pubip += data.toString();
-      });
-      eproc.stderr.on("data", (data) => {
-        console.error(colors.red("@error:"), data.toString());
-        res.status(500).json({ error: "Internal server error" });
-      });
-      ipProcess.stderr.on("data", (data) => {
-        console.error(colors.red("@error:"), data.toString());
-        res.status(500).json({ error: "Internal server error" });
-      });
-      eproc.on("close", () => {
-        ipProcess.on("close", () => {
-          const responseData = {
-            result: result.trim(),
-            pubip: pubip.trim(),
-          };
-          res.json(responseData);
-        });
-      });
-    } catch (error) {
-      console.error(colors.red("@error:"), error.message);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  }
-});
-
 function reproc() {
   const iproc = spawn("sh", [
     "-c",
