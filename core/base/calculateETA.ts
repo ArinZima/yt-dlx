3dffa28e1cd4e4b63589c35d6e16b1bb93883c5c
yt-dlx <yt-dlx@proton.me>
2024-03-21 09:06:57 +0530

    yt-dlx 5.22.0

--- core/base/calculateETA.ts
+++
@@ -1,6 +0,0 @@
-export default function calculateETA(startTime: any, percent: any) {
-  const currentTime: any = new Date();
-  const elapsedTime = (currentTime - startTime) / 1000;
-  const remainingTime = (elapsedTime / percent) * (100 - percent);
-  return remainingTime.toFixed(2);
-}
