e38962eaa08006ac694b0cff91aed80d17bc75a4
yt-dlx <yt-dlx@proton.me>
2024-03-21 08:11:18 +0530

    yt-dlx 5.21.0

--- core/__tests__/other/quick.spec.ts
+++ core/__tests__/other/quick.spec.ts
@@ -1,39 +1,49 @@
+// console.clear();
+// import AudioLowest from "../../pipes/Audio/single/AudioLowest";
+// import AudioHighest from "../../pipes/Audio/single/AudioHighest";
+// import VideoLowest from "../../pipes/Video/single/VideoLowest";
+// import VideoHighest from "../../pipes/Video/single/VideoHighest";
+
+// (async () => {
+// try {
+// await AudioLowest({
+// verbose: true,
+// onionTor: true,
+// output: "public/audio",
+// query: "https://youtu.be/pRLOXUlIUG0?si=dRXm_fVwubFrd4eI",
+// });
+
+// await AudioHighest({
+// verbose: true,
+// onionTor: true,
+// output: "public/audio",
+// query: "https://youtu.be/pRLOXUlIUG0?si=dRXm_fVwubFrd4eI",
+// });
+
+// await VideoLowest({
+// verbose: true,
+// onionTor: true,
+// output: "public/video",
+// query: "https://youtu.be/pRLOXUlIUG0?si=dRXm_fVwubFrd4eI",
+// });
+
+// await VideoHighest({
+// verbose: true,
+// onionTor: true,
+// output: "public/video",
+// query: "https://youtu.be/pRLOXUlIUG0?si=dRXm_fVwubFrd4eI",
+// });
+// } catch (error) {
+// console.error(error);
+// }
+// })();
+// ==========================================================
 console.clear();
-import AudioLowest from "../../pipes/audio/single/AudioLowest";
-import AudioHighest from "../../pipes/audio/single/AudioHighest";
-import VideoLowest from "../../pipes/video/single/VideoLowest";
-import VideoHighest from "../../pipes/video/single/VideoHighest";
+import list_formats from "../../pipes/command/list_formats";
 
 (async () => {
-  try {
-    await AudioLowest({
-      verbose: true,
-      onionTor: true,
-      output: "public/audio",
-      query: "https://youtu.be/pRLOXUlIUG0?si=dRXm_fVwubFrd4eI",
-    });
-
-    await AudioHighest({
-      verbose: true,
-      onionTor: true,
-      output: "public/audio",
-      query: "https://youtu.be/pRLOXUlIUG0?si=dRXm_fVwubFrd4eI",
-    });
-
-    await VideoLowest({
-      verbose: true,
-      onionTor: true,
-      output: "public/video",
-      query: "https://youtu.be/pRLOXUlIUG0?si=dRXm_fVwubFrd4eI",
-    });
-
-    await VideoHighest({
-      verbose: true,
-      onionTor: true,
-      output: "public/video",
-      query: "https://youtu.be/pRLOXUlIUG0?si=dRXm_fVwubFrd4eI",
-    });
-  } catch (error) {
-    console.error(error);
-  }
+  await list_formats({
+    onionTor: true,
+    query: "8k dolby nature",
+  });
 })();
--- core/index.ts
+++ core/index.ts
@@ -1,18 +1,21 @@
 import web from "./web";
 import help from "./pipes/command/help";
 import extract from "./pipes/command/extract";
-// import list_formats from "./pipes/command/list_formats";
+import list_formats from "./pipes/command/list_formats";
 import extract_playlist_videos from "./pipes/command/extract_playlist_videos";
 
-import AudioLowest from "./pipes/audio/single/AudioLowest";
-import AudioHighest from "./pipes/audio/single/AudioHighest";
-import ListAudioLowest from "./pipes/audio/list/ListAudioLowest";
-import ListAudioHighest from "./pipes/audio/list/ListAudioHighest";
+import AudioLowest from "./pipes/Audio/single/AudioLowest";
+import AudioHighest from "./pipes/Audio/single/AudioHighest";
+import ListAudioLowest from "./pipes/Audio/list/ListAudioLowest";
+import ListAudioHighest from "./pipes/Audio/list/ListAudioHighest";
 
-import VideoLowest from "./pipes/video/single/VideoLowest";
-import VideoHighest from "./pipes/video/single/VideoHighest";
-import ListVideoLowest from "./pipes/video/list/ListVideoLowest";
-import ListVideoHighest from "./pipes/video/list/ListVideoHighest";
+import VideoLowest from "./pipes/Video/single/VideoLowest";
+import VideoHighest from "./pipes/Video/single/VideoHighest";
+import ListVideoLowest from "./pipes/Video/list/ListVideoLowest";
+import ListVideoHighest from "./pipes/Video/list/ListVideoHighest";
+
+import AudioVideoHighest from "./pipes/AudioVideo/single/AudioVideoHighest";
+import AudioVideoLowest from "./pipes/AudioVideo/single/AudioVideoLowest";
 
 const ytdlx = {
   search: {
@@ -32,7 +35,7 @@ const ytdlx = {
   info: {
     help,
     extract,
-    // list_formats,
+    list_formats,
     extract_playlist_videos,
   },
   AudioOnly: {
@@ -54,6 +57,13 @@ const ytdlx = {
       Lowest: ListVideoLowest,
       Highest: ListVideoHighest,
     },
+  },
+  AudioVideo: {
+    Single: {
+      Lowest: AudioVideoLowest,
+      Highest: AudioVideoHighest,
+    },
+    List: {},
   },
 };
 export default ytdlx;
