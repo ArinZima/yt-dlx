5e7819d881c691e06857795d2465952e7f17ab88
yt-dlx <yt-dlx@proton.me>
2024-03-21 09:14:40 +0530

    yt-dlx 6.0.0

--- shared/typings/base/Agent.d.ts
+++
@@ -1,6 +0,0 @@
-import type { EngineOutput } from "./Engine";
-export default function Agent({ query, verbose, onionTor, }: {
-    query: string;
-    verbose?: boolean;
-    onionTor?: boolean;
-}): Promise<EngineOutput>;
