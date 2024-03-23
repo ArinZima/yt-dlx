79d9114d7422990806da5ac31e60ff028bd16e2d
yt-dlx <yt-dlx@proton.me>
2024-03-23 08:53:45 +0530

    yt-dlx 6.8.0

--- core/__tests__/Audio/single.highest.test.ts
+++ core/__tests__/Audio/single.highest.test.ts
@@ -1,16 +1,31 @@
+import * as fs from "fs";
 import ytdlx from "../..";
 import colors from "colors";
 
 (async () => {
   try {
-    console.log(colors.blue("@test:"), "Highest audio");
+    console.log(colors.blue("@test:"), "Download Highest audio");
     await ytdlx.AudioOnly.Single.Highest({
       stream: false,
       verbose: true,
-      onionTor: true,
+      onionTor: false,
       output: "public/audio",
       query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
     });
+
+    console.log(colors.blue("@test:"), "(stream) Download Highest audio");
+    const result = await ytdlx.AudioOnly.Single.Highest({
+      stream: true,
+      verbose: true,
+      onionTor: false,
+      output: "public/audio",
+      query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+    });
+    if (result && result.filename && result.ffmpeg) {
+      result.ffmpeg.pipe(fs.createWriteStream(result.filename));
+    } else {
+      console.error(colors.red("@error:"), "ffmpeg or filename not found!");
+    }
   } catch (error: any) {
     console.error(colors.red(error.message));
   }
