a1544aa9f569984950666c624198a7cd203099d3
yt-dlx <yt-dlx@proton.me>
2024-03-23 16:10:03 +0530

    yt-dlx @beta

--- util/dev.mjs
+++ util/dev.mjs
@@ -10,47 +10,40 @@ const colors = {
 };
 
 const core = {
-  remake: "yarn clean && yarn make && yarn update && yarn build",
-  postinstall: "run-s download-files setup-permissions install-chrome",
-  prepublishOnly: "yarn clean:deps",
+  "remake": "yarn clean && yarn make && yarn update && yarn build",
+  "postinstall": "run-s download-files setup-permissions install-chrome",
+  "prepublishOnly": "yarn clean:deps",
   "setup-permissions": "chmod -R +x util/*",
   "download-files": "node util/engine.mjs",
   "install-chrome": "npx puppeteer browsers install chrome",
   "ui:dev": "cd client && yarn dev",
   "ui:build": "cd client && yarn build",
   "ui:start": "cd client && yarn start",
-  clean: "yarn clean:base && yarn clean:client && yarn clean:deps",
+  "clean": "yarn clean:base && yarn clean:client && yarn clean:deps",
   "clean:base": "rm -rf node_modules temp dist others",
   "clean:client": "cd client && rm -rf node_modules .next",
   "clean:deps": "rm -rf util/ffmpeg.tar.xz util/ffmpeg util/engine",
-  make: "yarn make:base && yarn make:client",
+  "make": "yarn make:base && yarn make:client",
   "make:base": "yarn install --verbose",
   "make:client": "cd client && yarn install --verbose",
-  update: "yarn update:base && yarn update:client",
+  "update": "yarn update:base && yarn update:client",
   "update:base": "yarn install --verbose && yarn upgrade --latest",
-  "update:client":
-    "cd client && yarn install --verbose && yarn upgrade --latest",
+  "update:client": "cd client && yarn install --verbose && yarn upgrade --latest",
   "tsc:cjs": "tsc -p ./config/cjs.json",
   "tsc:esm": "tsc -p ./config/esm.json",
   "tsc:types": "tsc -p ./config/types.json",
-  build: "yarn build:base && yarn build:client",
+  "build": "yarn build:base && yarn build:client",
   "build:client": "cd client && rm -rf .next temp && yarn build",
-  "build:base":
-    "rm -rf dist temp && yarn tsc:cjs && yarn tsc:esm && yarn tsc:types",
-  spec: "rm -rf temp && tsup core/__tests__/other/quick.spec.ts --outDir temp && node temp/quick.spec.js",
-  test: "rm -rf temp && yarn test:scrape && yarn test:mix && yarn test:video && yarn test:audio && yarn test:command && yarn test:cli",
-  "test:mix":
-    "rm -rf temp && tsup core --outDir temp && node temp/__tests__/mix.js",
-  "test:video":
-    "rm -rf temp && tsup core --outDir temp && node temp/__tests__/video.js",
-  "test:audio":
-    "rm -rf temp && tsup core --outDir temp && node temp/__tests__/audio.js",
-  "test:command":
-    "rm -rf temp && tsup core --outDir temp && node temp/__tests__/command.js",
-  "test:scrape":
-    "rm -rf temp && tsup core/__tests__/other/scrape.spec.ts --outDir temp && node temp/scrape.spec.js",
-  "test:cli":
-    "yarn link && yt-dlx audio-lowest --query PERSONAL BY PLAZA && yt-dlx al --query SuaeRys5tTc && yarn unlink",
+  "build:base": "rm -rf dist temp && yarn tsc:cjs && yarn tsc:esm && yarn tsc:types",
+  "spec": "rm -rf temp && tsup core/__tests__/other/quick.spec.ts --outDir temp && node temp/quick.spec.js",
+  "test": "rm -rf temp && test:pkg && yarn test:scrape && yarn test:mix && yarn test:video && yarn test:audio && yarn test:command && yarn test:cli",
+  "test:pkg": "cd pkg && yarn test",
+  "test:mix": "rm -rf temp && tsup core --outDir temp && node temp/__tests__/mix.js",
+  "test:video": "rm -rf temp && tsup core --outDir temp && node temp/__tests__/video.js",
+  "test:audio": "rm -rf temp && tsup core --outDir temp && node temp/__tests__/audio.js",
+  "test:command": "rm -rf temp && tsup core --outDir temp && node temp/__tests__/command.js",
+  "test:scrape": "rm -rf temp && tsup core/__tests__/other/scrape.spec.ts --outDir temp && node temp/scrape.spec.js",
+  "test:cli": "yarn link && yt-dlx audio-lowest --query PERSONAL BY PLAZA && yt-dlx al --query SuaeRys5tTc && yarn unlink"
 };
 function formatBytes(bytes) {
   const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
