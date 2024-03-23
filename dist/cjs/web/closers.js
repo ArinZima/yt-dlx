c72f39aa1832f5cac73c842c2f1d9691e3e5f0b5
yt-dlx <yt-dlx@proton.me>
2024-03-23 16:11:24 +0530

    yt-dlx @beta

--- dist/cjs/web/closers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function closers(browser) {
-    const pages = await browser.pages();
-    await Promise.all(pages.map((page) => page.close()));
-    await browser.close();
-}
-exports.default = closers;
