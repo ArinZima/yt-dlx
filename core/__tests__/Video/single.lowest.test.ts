e1b14ab1f518092edcdecb41e23742de1f79ddf6
yt-dlx <yt-dlx@proton.me>
2024-03-25 08:03:23 +0530

    yt-dlx stable @8.0.1

---
+++ core/__tests__/Video/single.lowest.test.ts
@@ -0,0 +1,32 @@
+import * as fs from "fs";
+import ytdlx from "../..";
+import colors from "colors";
+
+(async () => {
+  try {
+    console.log(colors.blue("@test:"), "Download Lowest audio");
+    await ytdlx.VideoOnly.Single.Lowest({
+      stream: false,
+      verbose: true,
+      onionTor: false,
+      output: "public/audio",
+      query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+    });
+
+    console.log(colors.blue("@test:"), "(stream) Download Lowest audio");
+    const result = await ytdlx.VideoOnly.Single.Lowest({
+      stream: true,
+      verbose: true,
+      onionTor: false,
+      output: "public/audio",
+      query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+    });
+    if (result && result.filename && result.ffmpeg) {
+      result.ffmpeg.pipe(fs.createWriteStream(result.filename));
+    } else {
+      console.error(colors.red("@error:"), "ffmpeg or filename not found!");
+    }
+  } catch (error: any) {
+    console.error(colors.red(error.message));
+  }
+})();
---
+++ pkg/ytdlx.ts
@@ -0,0 +1,6 @@
+import ytdlx from "yt-dlx";
+import colors from "colors";
+
+console.info(colors.blue("@info:"), "running ytdlx.ts");
+console.info(colors.blue("@info:"), "yt-dlx structure");
+console.info(colors.blue("@info:"), ytdlx);
