007663da7a7411d7050c9770b6d584ef6d006079
yt-dlx <yt-dlx@proton.me>
2024-03-25 08:03:11 +0530

    yt-dlx stable @8.0.1

--- pkg/ytdlx.ts
+++
@@ -1,6 +0,0 @@
-import ytdlx from "yt-dlx";
-import colors from "colors";
-
-console.info(colors.blue("@info:"), "running ytdlx.ts");
-console.info(colors.blue("@info:"), "yt-dlx structure");
-console.info(colors.blue("@info:"), ytdlx);
