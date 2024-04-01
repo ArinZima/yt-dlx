4cae63e18f752eee057042d7ac53d5db7de0ea53
yt-dlx <yt-dlx@proton.me>
2024-04-01 21:18:26 +0530

    yt-dlx 8.0.5 stable

--- backend/app/pkg/Audio/AudioHighest/page.tsx
+++ backend/app/pkg/Audio/AudioHighest/page.tsx
@@ -285,7 +285,7 @@ export default function AwesomePackage() {
           <article className="space-y-8">
             <div className="space-y-6">
               <h1 className="text-5xl text-red-600 font-bold lg:text-9xl">
-                YT-DLX@8.0.4
+                YT-DLX@8.0.5
               </h1>
             </div>
             <p className="text-gray-400">
