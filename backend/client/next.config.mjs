ea657daf22e32c20b1f13360c99cdae754372bfc
yt-dlx <yt-dlx@proton.me>
2024-04-02 13:27:30 +0530

    yt-dlx 8.0.8

---
+++ backend/client/next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+    ],
+  },
+};
+export default nextConfig;
