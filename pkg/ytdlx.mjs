db619bfe177e25389b8245c61878405bbccc0b8d
yt-dlx <yt-dlx@proton.me>
2024-04-02 08:55:37 +0530

    yt-dlx 8.0.6 stable

--- pkg/ytdlx.mjs
+++
@@ -1,6 +0,0 @@
-import colors from "colors";
-import ytdlx from "yt-dlx";
-
-console.info(colors.green("@info:"), "running ytdlx.mjs");
-console.info(colors.green("@info:"), "yt-dlx structure");
-console.info(colors.green("@info:"), ytdlx.default);
