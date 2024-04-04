042ffeb45bd6b6583e3434ada434263f57614a13
yt-dlx <yt-dlx@proton.me>
2024-04-04 19:39:28 +0530

    yt-dlx 8.1.0

---
+++ dist/esm/core/base/Agent.js
@@ -0,0 +1,54 @@
+import colors from "colors";
+import { version } from "../../package.json";
+import web from "../web";
+import YouTubeID from "../web/YouTubeId";
+import Engine from "./Engine";
+import niptor from "./niptor";
+export default async function Agent({ query, verbose, onionTor, }) {
+    console.log(colors.green("@info:"), "using", colors.green("yt-dlx"), "version", colors.green(version));
+    let ipAddress;
+    let nipTor;
+    nipTor = await niptor(["curl https://checkip.amazonaws.com --insecure"]);
+    console.log(colors.green("@info:"), "verbose", colors.green(`${verbose}`));
+    console.log(colors.green("@info:"), "system", colors.green("ipAddress"), nipTor.stdout.trim());
+    ipAddress = nipTor.stdout.trim();
+    if (onionTor) {
+        nipTor = await niptor([
+            "systemctl restart tor && curl --socks5-hostname 127.0.0.1:9050 https://checkip.amazonaws.com --insecure",
+        ]);
+        if (nipTor.stdout.trim().length > 0) {
+            console.log(colors.green("@info:"), "socks5", colors.green("ipAddress"), nipTor.stdout.trim());
+            ipAddress = nipTor.stdout.trim();
+        }
+        else
+            throw new Error("Unable to connect to Tor.");
+    }
+    let TubeBody;
+    let respEngine = undefined;
+    let videoId = await YouTubeID(query);
+    if (!videoId) {
+        TubeBody = await web.browserLess.searchVideos({ query });
+        if (!TubeBody[0])
+            throw new Error("Unable to get response!");
+        console.log(colors.green("@info:"), `preparing payload for`, colors.green(TubeBody[0].title));
+        respEngine = await Engine({
+            onionTor,
+            ipAddress,
+            query: `https://www.youtube.com/watch?v=${TubeBody[0].id}`,
+        });
+        return respEngine;
+    }
+    else {
+        TubeBody = await web.browserLess.singleVideo({ videoId });
+        if (!TubeBody)
+            throw new Error("Unable to get response!");
+        console.log(colors.green("@info:"), `preparing payload for`, colors.green(TubeBody.title));
+        respEngine = await Engine({
+            onionTor,
+            ipAddress,
+            query: `https://www.youtube.com/watch?v=${TubeBody.id}`,
+        });
+        return respEngine;
+    }
+}
+//# sourceMappingURL=Agent.js.map
