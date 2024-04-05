0f6b2867944319bfb43ca1ac906dc228c49006fa
yt-dlx <yt-dlx@proton.me>
2024-04-05 08:52:29 +0530

    yt-dlx 8.1.0

---
+++ dist/types/web/index.d.ts
@@ -0,0 +1,34 @@
+import PlaylistInfo from "./browser/PlaylistInfo";
+import type { PlaylistInfoType } from "./browser/PlaylistInfo";
+import SearchVideos from "./browser/SearchVideos";
+import type { TypePlaylist, TypeVideo } from "./browser/SearchVideos";
+import VideoInfo from "./browser/VideoInfo";
+import type { VideoInfoType } from "./browser/VideoInfo";
+import playlistVideos from "./vercel/playlistVideos";
+import type { playlistVideosType } from "./vercel/playlistVideos";
+import relatedVideos from "./vercel/relatedVideos";
+import type { relatedVideosType } from "./vercel/relatedVideos";
+import searchPlaylists from "./vercel/searchPlaylists";
+import type { searchPlaylistsType } from "./vercel/searchPlaylists";
+import searchVideos from "./vercel/searchVideos";
+import type { searchVideosType } from "./vercel/searchVideos";
+import singleVideo from "./vercel/singleVideo";
+import type { singleVideoType } from "./vercel/singleVideo";
+export type { TypeVideo, TypePlaylist, VideoInfoType, PlaylistInfoType };
+export type { singleVideoType, searchVideosType, relatedVideosType, playlistVideosType, searchPlaylistsType, };
+declare const web: {
+    browser: {
+        VideoInfo: typeof VideoInfo;
+        SearchVideos: typeof SearchVideos;
+        PlaylistInfo: typeof PlaylistInfo;
+    };
+    browserLess: {
+        singleVideo: typeof singleVideo;
+        searchVideos: typeof searchVideos;
+        relatedVideos: typeof relatedVideos;
+        playlistVideos: typeof playlistVideos;
+        searchPlaylists: typeof searchPlaylists;
+    };
+};
+export default web;
+//# sourceMappingURL=index.d.ts.map
