6880bd2b6d6d774a5af078b9cbc6473cb6de42b8
yt-dlx <yt-dlx@proton.me>
2024-04-05 14:59:28 +0530

    yt-dlx 8.1.0

--- cluster/client/app/api/audio/single/highest/route.ts
+++ cluster/client/app/api/audio/single/highest/route.ts
@@ -1,10 +1,28 @@
 import proTube from "yt-dlx";
 
 export async function GET(request: Request): Promise<Response> {
-  return new Promise(async (resolve, reject) => {
-    const url = new URL(request.url);
-    const formdata = url.searchParams.get("formdata");
-    console.log(formdata);
+  return new Promise((resolve, reject) => {
+    switch (true) {
+      case !!"undefined":
+        resolve(
+          new Response("error", {
+            status: 200,
+          })
+        );
+        break;
+      default:
+        reject(
+          new Response("error", {
+            status: 200,
+          })
+        );
+        break;
+    }
+  });
+}
+
+export async function POST(request: Request): Promise<Response> {
+  return new Promise((resolve, reject) => {
     switch (true) {
       case !!"undefined":
         resolve(
