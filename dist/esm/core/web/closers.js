4ebc474d0cf4512c2e1b61f6869b2afd609e4914
yt-dlx <yt-dlx@proton.me>
2024-04-05 15:30:37 +0530

    yt-dlx 8.1.0

--- dist/esm/core/web/closers.js
+++
@@ -1,6 +0,0 @@
-export default async function closers(browser) {
-    const pages = await browser.pages();
-    await Promise.all(pages.map((page) => page.close()));
-    await browser.close();
-}
-//# sourceMappingURL=closers.js.map
