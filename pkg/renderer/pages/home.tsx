7295b4105d9d497e2e8ba65b708aa278ea75bdd0
yt-dlx <yt-dlx@proton.me>
2024-04-07 01:43:40 +0530

    yt-dlx 8.0.2

--- pkg/renderer/pages/home.tsx
+++ pkg/renderer/pages/home.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from "react";
+import Head from "next/head";
 
 export default function HomePage() {
   return (
@@ -9,29 +7,6 @@ export default function HomePage() {
       <Head>
         <title>Home - Nextron (with-tailwindcss)</title>
       </Head>
-      <div className="grid grid-col-1 text-2xl w-full text-center">
-        <div>
-          <Image
-            className="ml-auto mr-auto"
-            src="/images/logo.png"
-            alt="Logo image"
-            width="256px"
-            height="256px"
-          />
-        </div>
-        <span>⚡ Electron ⚡</span>
-        <span>+</span>
-        <span>Next.js</span>
-        <span>+</span>
-        <span>tailwindcss</span>
-        <span>=</span>
-        <span>💕 </span>
-      </div>
-      <div className="mt-1 w-full flex-wrap flex justify-center">
-        <Link href="/next">
-          <a className="btn-blue">Go to next page</a>
-        </Link>
-      </div>
     </React.Fragment>
-  )
+  );
 }
