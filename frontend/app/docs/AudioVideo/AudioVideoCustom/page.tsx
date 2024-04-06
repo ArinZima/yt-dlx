5873fc25c2547485bf62a3426d33f10e20f25a27
yt-dlx <yt-dlx@proton.me>
2024-04-06 18:17:09 +0530

    yt-dlx 8.1.1

--- frontend/app/docs/AudioVideo/AudioVideoCustom/page.tsx
+++ frontend/app/docs/AudioVideo/AudioVideoCustom/page.tsx
@@ -148,7 +148,7 @@ export default function AwesomePackage({ param }: any) {
                     </p>
                     <div className="mt-6">
                       <a
-                        href="#"
+                        href="https://github.com/yt-dlx/yt-dlx/blob/main/examples/typescript/Audio/single.custom.test.ts"
                         className="flex items-center justify-center w-full px-10 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-red-900 rounded-xl hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                       >
                         View profile
@@ -179,7 +179,7 @@ export default function AwesomePackage({ param }: any) {
                     </p>
                     <div className="mt-6">
                       <a
-                        href="#"
+                        href="https://github.com/yt-dlx/yt-dlx/blob/main/examples/javascript/Audio/single.custom.test.js"
                         className="flex items-center justify-center w-full px-10 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-red-900 rounded-xl hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                       >
                         View profile
@@ -210,7 +210,7 @@ export default function AwesomePackage({ param }: any) {
                     </p>
                     <div className="mt-6">
                       <a
-                        href="#"
+                        href="https://github.com/yt-dlx/yt-dlx/blob/main/examples/ecmascript/Audio/single.custom.test.mjs"
                         className="flex items-center justify-center w-full px-10 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-red-900 rounded-xl hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                       >
                         View profile
