eec2949b8ed303f6558cf18e2793ed4276834efb
yt-dlx <yt-dlx@proton.me>
2024-04-06 11:18:45 +0530

    yt-dlx 8.1.0

---
+++ project/esm/core/__tests__/Command/search_playlists.test.js
@@ -0,0 +1,15 @@
+import ytdlx from "../..";
+import colors from "colors";
+(async () => {
+    try {
+        console.log(colors.blue("@test:"), "ytSearch playlist multiple");
+        const result = await ytdlx.ytSearch.Playlist.Multiple({
+            query: "8k dolby nature",
+        });
+        console.log(result);
+    }
+    catch (error) {
+        console.error(colors.red(error.message));
+    }
+})();
+//# sourceMappingURL=search_playlists.test.js.map
---
+++ project/esm/core/__tests__/Command/video_data.test.js
@@ -0,0 +1,15 @@
+import ytdlx from "../..";
+import colors from "colors";
+(async () => {
+    try {
+        console.log(colors.blue("@test:"), "ytSearch video single");
+        const result = await ytdlx.ytSearch.Video.Single({
+            query: "https://www.youtube.com/watch?v=AbFnsaDQMYQ",
+        });
+        console.log(result);
+    }
+    catch (error) {
+        console.error(colors.red(error.message));
+    }
+})();
+//# sourceMappingURL=video_data.test.js.map
