6102c045a50b2b9a0e043364fdda90b56249f608
yt-dlx <yt-dlx@proton.me>
2024-04-06 17:33:37 +0530

    yt-dlx 8.1.0

--- examples/typescript/AudioVideo/single.lowest.test.ts
+++ examples/typescript/AudioVideo/single.lowest.test.ts
@@ -1,3 +1,16 @@
+// ======================================[ DOCUMENTATION ]======================================
+/**
+ * Downloads and processes a single YouTube video with audio customization options.
+ *
+ * @param query - The YouTube video URL or ID or name.
+ * @param output - (optional) The output directory for the processed file.
+ * @param stream - (optional) Whether to stream the processed video or not.
+ * @param filter - (optional) The audio filter to apply. Available options: "echo", "slow", "speed", "phaser", "flanger", "panning", "reverse", "vibrato", "subboost", "surround", "bassboost", "nightcore", "superslow", "vaporwave", "superspeed".
+ * @param verbose - (optional) Whether to log verbose output or not.
+ * @param onionTor - (optional) Whether to use Tor for the download or not.
+ * @param resolution - The desired audio resolution. Available options: "high", "medium", "low", "ultralow".
+ * @returns A Promise that resolves with either `void` (if `stream` is false) or an object containing the `ffmpeg` instance and the output filename (if `stream` is true).
+ */
 // =============================[ USING YT-DLX'S DOWNLOAD MACHANISM ]=============================
 //
 import colors from "colors";
