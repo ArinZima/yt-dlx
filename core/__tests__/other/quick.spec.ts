452a989e5c60533f29a9ac7ae3c1c45372258288
yt-dlx <yt-dlx@proton.me>
2024-04-06 20:11:14 +0530

    yt-dlx 8.1.1

--- core/__tests__/other/quick.spec.ts
+++ core/__tests__/other/quick.spec.ts
@@ -62,7 +62,6 @@ export default async function AudioHighest({
       const ff: FfmpegCommand = ffmpeg();
       ff.addInput(engineData.AudioHighF.url);
       ff.addInput(engineData.metaData.thumbnail);
-      ff.audioCodec("copy");
       ff.withOutputFormat("avi");
       ff.addOption("-headers", "X-Forwarded-For: " + engineData.ipAddress);
       switch (filter) {
