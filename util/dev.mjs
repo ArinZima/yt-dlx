c7473431ab31149c2165023ece35443e1f778c79
yt-dlx <yt-dlx@proton.me>
2024-04-07 02:21:09 +0530

    yt-dlx 8.0.2

--- util/dev.mjs
+++ util/dev.mjs
@@ -12,34 +12,34 @@ const colors = {
 
 const core = {
   remake:
-    "pnpm run clean && pnpm run make && pnpm run update && pnpm run build",
+    "yarn run clean && yarn run make && yarn run update && yarn run build",
   postinstall: "run-s download-files setup-permissions install-chrome",
-  prepublishOnly: "pnpm run clean:deps",
+  prepublishOnly: "yarn run clean:deps",
   "setup-permissions": "chmod -R +x util/*",
   "download-files": "node util/cprobe.mjs",
   "install-chrome": "npx puppeteer browsers install chrome",
-  "frontend:dev": "cd frontend && pnpm run dev",
-  "frontend:lint": "cd frontend && pnpm run lint",
-  "frontend:build": "cd frontend && pnpm run build",
-  "frontend:start": "cd frontend && pnpm run start",
-  "frontend:clean": "cd frontend && pnpm run clean",
+  "frontend:dev": "cd frontend && yarn run dev",
+  "frontend:lint": "cd frontend && yarn run lint",
+  "frontend:build": "cd frontend && yarn run build",
+  "frontend:start": "cd frontend && yarn run start",
+  "frontend:clean": "cd frontend && yarn run clean",
   clean:
-    "pnpm run clean:base && pnpm run clean:frontend && pnpm run clean:deps",
+    "yarn run clean:base && yarn run clean:frontend && yarn run clean:deps",
   "clean:base": "rm -rf node_modules temp project others",
-  "clean:frontend": "cd frontend && pnpm run clean",
+  "clean:frontend": "cd frontend && yarn run clean",
   "clean:deps": "rm -rf util/ffmpeg.tar.xz util/ffmpeg util/cprobe",
-  make: "pnpm run make:base && pnpm run make:frontend",
-  "make:base": "pnpm install",
-  "make:frontend": "cd frontend && pnpm install",
-  update: "pnpm run update:base && pnpm run update:frontend",
-  "update:base": "pnpm up --latest",
-  "update:frontend": "cd frontend && pnpm up --latest",
-  build: "pnpm run build:base && pnpm run build:frontend",
-  "build:frontend": "cd frontend && pnpm run clean && pnpm run build",
+  make: "yarn run make:base && yarn run make:frontend",
+  "make:base": "yarn install",
+  "make:frontend": "cd frontend && yarn install",
+  update: "yarn run update:base && yarn run update:frontend",
+  "update:base": "yarn upgrade --latest",
+  "update:frontend": "cd frontend && yarn upgrade --latest",
+  build: "yarn run build:base && yarn run build:frontend",
+  "build:frontend": "cd frontend && yarn run clean && yarn run build",
   "build:base":
     "rm -rf project temp && tsc -p ./config/cjs.json && tsc -p ./config/esm.json && tsc -p ./config/types.json",
   spec: "rm -rf temp && tsup core/__tests__/other/quick.spec.ts --outDir temp && node temp/quick.spec.js",
-  test: "rm -rf temp && pnpm run test:scrape && pnpm run test:mix && pnpm run test:video && pnpm run test:audio && pnpm run test:command && pnpm run test:cli",
+  test: "rm -rf temp && yarn run test:scrape && yarn run test:mix && yarn run test:video && yarn run test:audio && yarn run test:command && yarn run test:cli",
   "test:mix":
     "rm -rf temp && tsup core --outDir temp && node temp/__tests__/mix.js",
   "test:video":
@@ -51,7 +51,7 @@ const core = {
   "test:scrape":
     "rm -rf temp && tsup core/__tests__/other/scrape.spec.ts --outDir temp && node temp/scrape.spec.js",
   "test:cli":
-    "pnpm run link && yt-dlx audio-lowest --query PERSONAL BY PLAZA && yt-dlx al --query SuaeRys5tTc && pnpm run unlink",
+    "yarn run link && yt-dlx audio-lowest --query PERSONAL BY PLAZA && yt-dlx al --query SuaeRys5tTc && yarn run unlink",
 };
 function formatBytes(bytes) {
   const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
