74651a0cb9411442b922411001a5d6a8b8d47266
yt-dlx <yt-dlx@proton.me>
2024-04-07 08:15:33 +0530

    yt-dlx 8.0.2

--- pkg/src/renderer/src/App.tsx
+++ pkg/src/renderer/src/App.tsx
@@ -1,14 +1,16 @@
-function App(): JSX.Element {
+import "./index.css"
+
+export default function App() {
   return (
     <div className="bg-neutral-800 h-screen flex items-center justify-center">
       <section className="text-white font-bold p-8 max-w-lg">
         <h1 className="text-3xl mb-4">Welcome to Electron-Vite TailwindCSS App</h1>
         <p className="text-lg">
-          Magna officia irure irure ipsum ex nisi quis pariatur. Labore ex quis dolor amet minim nisi ipsum pariatur ad esse voluptate. Ea excepteur ea est irure eu velit id laborum proident id excepteur.
+          Magna officia irure irure ipsum ex nisi quis pariatur. Labore ex quis dolor amet minim
+          nisi ipsum pariatur ad esse voluptate. Ea excepteur ea est irure eu velit id laborum
+          proident id excepteur.
         </p>
       </section>
     </div>
-  );
+  )
 }
-
-export default App;
