944db79d5f1084c3aeda463f9c908a3f256a3efc
yt-dlx <yt-dlx@proton.me>
2024-04-07 08:03:37 +0530

    yt-dlx 8.0.2

--- pkg/src/renderer/src/main.tsx
+++ pkg/src/renderer/src/main.tsx
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import GlobalProviders from "./providers"
+import ReactDOM from "react-dom/client"
+import "./assets/main.css"
+import React from "react"
+import App from "./App"
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <GlobalProviders>
+      <App />
+    </GlobalProviders>
   </React.StrictMode>
 )
