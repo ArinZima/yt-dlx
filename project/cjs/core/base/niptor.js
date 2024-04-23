1a4759fb342d786cb637f3be45535abf79d1d64a
Arin Zima <110127554+ArinZima@users.noreply.github.com>
2024-04-23 14:26:36 +0200

    Added error logging for checkSudo()

--- project/cjs/core/base/niptor.js
+++ project/cjs/core/base/niptor.js
@@ -5,19 +5,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const colors_1 = __importDefault(require("colors"));
 const child_process_1 = require("child_process");
+/**
+ * Checks if sudo is available.
+ *
+ * @returns A Promise that resolves with a boolean indicating whether sudo is available.
+ */
 async function checkSudo() {
     return new Promise((resolve) => {
-        if (process.env.sudo === "false")
-            resolve(false);
-        else {
-            const check = (0, child_process_1.spawn)("sudo", ["-n", "true"]);
-            check.on("error", () => resolve(false));
-            check.on("close", (code) => {
-                resolve(code === 0);
-            });
-        }
+        const check = (0, child_process_1.spawn)("sudo", ["-n", "true"]);
+        check.on("close", (code) => {
+            resolve(code === 0);
+        });
+        check.on("error", (error) => {
+            console.error(error);
+        });
     });
 }
+/**
+ * Executes a command with or without sudo based on availability.
+ *
+ * @param args - The arguments to pass to the command.
+ * @returns A Promise that resolves with an object containing stdout and stderr data.
+ * @throws An error if the command execution fails.
+ */
 async function niptor(args) {
     const sudoAvailable = await checkSudo();
     const command = sudoAvailable ? ["sudo", ...args] : args;
